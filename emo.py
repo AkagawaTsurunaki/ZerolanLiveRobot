d79df1d40d1362c4bb96fc9736a26bd568a03cc1
AkagawaTsurunaki <965145619@qq.com>
2024-03-07 14:29:45 +0800

    feat: 更新默认心情检索模板

--- emo.py
+++ emo.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 from dataclasses import dataclass
 from typing import List
@@ -20,8 +21,8 @@ class Emotion:
 emo_list: List[Emotion] = []
 
 
-def load_emo_list(tts_name: str, emo_list_file_path='gptsovits/prompts/default.yaml'):
-    if tts_name == gptsovits.__name__:
+def load_emo_list(tts_serv_name: str, emo_list_file_path='gptsovits/prompts/default.yaml'):
+    if tts_serv_name == gptsovits.__name__:
         assert os.path.exists(emo_list_file_path), f'音频提示路径不存在：{emo_list_file_path}'
         with open(file=emo_list_file_path, mode='r', encoding='utf-8') as file:
             prompt: dict = yaml.safe_load(file)
@@ -32,8 +33,8 @@ def load_emo_list(tts_name: str, emo_list_file_path='gptsovits/prompts/default.y
                 prompt_language = prompt[emotion]['prompt_language']
 
                 assert os.path.exists(refer_wav_path), f'提示音频路径 refer_wav_path 不存在：{refer_wav_path}'
-                assert prompt_text is None or prompt_text == '', f'提示文本 prompt_text 不能为空'
-                assert prompt_language not in ['zh', 'en', 'ja'], f'提示音频所代表的语言 {prompt_language} 是不被支持的'
+                assert not (prompt_text is None or prompt_text == ''), f'提示文本 prompt_text 不能为空'
+                assert prompt_language in ['zh', 'en', 'ja'], f'提示音频所代表的语言 {prompt_language} 是不被支持的'
 
                 e = Emotion(
                     id=emotion,
@@ -52,23 +53,40 @@ def ana_emo(llm_name: str, text: str):
     根据 text 分析其情感（目前仅支持ChatGLM3）
     如果不能分析出具体的情感，那么默认返回第一个情感
 
-    :param prompt: 提示词模板，需要包含 {text} {emo1} {emo2} ... 标签
+    :param prompt: 提示词模板，需要包含 {text} {emo_list} ... 标签
     :param llm_name: 模型名称（请使用模块名）
     :param text: 要分析的文本
     :return:
     """
-    assert emo_list and len(emo_list) > 0, 'emo_list 必须含有至少一种心情'
+    assert emo_list and len(emo_list) > 0, 'emo_list 必须含有至少一种心情 DEFAULT'
 
-    with open(file='template/emo_ana.txt', mode='r', encoding='utf-8') as file:
-        prompt = file.read()
+    with open(file='template/emo_ana.json', mode='r', encoding='utf-8') as file:
+        emo_ana_prompt = json.load(file)
+
+    # 使用 ChatGLM3 模型
 
     if llm_name == chatglm3.__name__:
+
         from chatglm3 import api
+        from chatglm3.api import ModelRequest
+
+        model_req = ModelRequest(**emo_ana_prompt)
+
+        # 模板提示替换关键词
+        model_req.query = model_req.query.replace('{text}', text)
+
+        emo_list_rep = ''
         for idx, emotion in enumerate(emo_list):
-            prompt = prompt.replace('{emo' + f'{idx}' + '}', emotion.id)
-        prompt = prompt.replace('{text}', text)
-        # 使用 ChatGLM3 模型
-        emotion_id = api.quick_chat(prompt)
+            emo_list_rep += f'{emotion.id},'
+            model_req.history[2 * idx + 1]['content'] = model_req.history[2 * idx + 1]['content'].replace('{emo_id}',
+                                                                                                          emotion.id)
+
+        model_req.history[0]['content'] = model_req.history[0]['content'].replace('emo_list', emo_list_rep)
+
+        # 向 ChatGLM3 查询心情 ID
+        emotion_id = api.quick_chat(model_req)
+
+        # 校验心情 ID 是否合法
         for emotion in emo_list:
             if emotion_id == emotion.id:
                 return emotion_id
