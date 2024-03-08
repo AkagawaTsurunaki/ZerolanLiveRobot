8a1dc0f514faccf56eb666bdaf5775ba1058d9dc
AkagawaTsurunaki <965145619@qq.com>
2024-03-08 15:43:47 +0800

    feat: 从文件中读取提示词，同时支持历史模式

--- controller.py
+++ controller.py
@@ -1,9 +1,11 @@
 import asyncio
+import json
 
 from loguru import logger
 
 import chatglm3
 import emo
+from emo import Emotion
 import gptsovits
 from audio_player import service as audio_player_serv
 from bilibili import service as bili_serv
@@ -13,6 +15,23 @@ from gptsovits import service as tts_serv
 # 控制死循环
 FLAG = True
 LANG = 'zh'
+
+
+def load_sys_prompt():
+    with open(file='template/game_prompt.json', mode='r', encoding='utf-8') as file:
+        json_value = json.load(file)
+        model_req = ModelRequest(**json_value)
+    return model_req
+
+
+default_model_req = load_sys_prompt()
+
+
+def write_output(danmaku, text: str, emotion: Emotion):
+    with open(file='.tmp/emotion_output/emotion.txt', mode='w', encoding='utf-8') as file:
+        file.write(emotion.id)
+    with open(file='.tmp/llm_output/chatglm3.txt', mode='w', encoding='utf-8') as file:
+        file.write(f'> {danmaku.username}: {danmaku.msg} \n[{emotion.id}] {text}')
 
 
 def is_blank(s: str):
@@ -31,12 +50,9 @@ def is_blank(s: str):
 
 
 async def circle():
-    # # 先确认当前是否有音频正在播放
-    # if not audio_player_serv.is_over():
-    #     return
-    #
+
     # 查看是否有可以选择的弹幕
-    danmaku = bili_serv.select_01(k=5)
+    danmaku = bili_serv.select_01(k=3)
 
     if not danmaku:
         return
@@ -44,26 +60,9 @@ async def circle():
 
     # 封装为一个模型请求体
 
-    sys_prompt = """
-    你现在是一名叫幼刀丛雨的猫娘，正在哔哩哔哩上直播，请与观众们友好互动。
-    以下是弹幕的格式
-    '''
-    [用户名] 弹幕所说的话
-    '''
-    请注意你的回复不能超过超过50字，对话的情感要丰富，有时生气，有时开心，有时傲娇，有时搞怪。
-    """
-    query = f'[{danmaku.username}] {danmaku.msg}'
+    default_model_req.sys_prompt = ''
+    default_model_req.query = f'[{danmaku.username}] {danmaku.msg}'
 
-    # TODO: 历史记忆
-    model_req = ModelRequest(
-        sys_prompt=sys_prompt,
-        query=query,
-        history=[],
-        temperature=1.,
-        top_p=1.
-    )
-
-    # 有弹幕可读
     # 其中 resp
     # 第1轮循环 resp = '我'
     # 第2轮循环 resp = '我是'
@@ -75,7 +74,7 @@ async def circle():
 
     now = ''
 
-    for model_resp in stream_chat(model_req):
+    for model_resp in stream_chat(default_model_req):
         resp = model_resp.response
         # 按照标点符号切割句子
         if not resp:
@@ -88,15 +87,29 @@ async def circle():
         sentence = resp[len(now): len(resp)]
         now = now + sentence
 
-        # TTS 任务
         if is_blank(sentence):
             continue
+
+        if len(sentence) < 6:
+            continue
+
+        # 更新历史
+
+        default_model_req.history = model_resp.history
+        logger.debug(f'当前 LLM 历史记录：{len(default_model_req.history)}')
 
         # 分析句子的情感倾向
         emotion = emo.ana_emo(chatglm3.__name__, sentence)
         logger.info(f'心情：{emotion.id}')
+
+        # 根据心情切换 Prompt
         tts_serv.change_prompt(emotion.refer_wav_path, emotion.prompt_text, emotion.prompt_language)
+
+        # TTS 任务
         wav_file_path = tts_serv.predict(sentence, LANG)
+
+        # 写入心情
+        write_output(danmaku, sentence, emotion)
 
         # 如果音频文件不为空（如果服务器出错，则为空），则播放音频
         if not wav_file_path:
