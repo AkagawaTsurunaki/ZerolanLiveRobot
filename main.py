7baa4564d0e26d0b19f76df84a5572cfbe95e528
AkagawaTsurunaki <965145619@qq.com>
2024-03-11 21:31:52 +0800

    feat: API 服务连接失败时的报错更加用户友好

--- main.py
+++ main.py
@@ -34,27 +34,29 @@ async def service_start():
 
 if __name__ == '__main__':
     # 加载配置文件
+    try:
+        config = load_global_config(DEFAULT_GLOBAL_CONFIG_PATH)
 
-    config = load_global_config(DEFAULT_GLOBAL_CONFIG_PATH)
+        bilibili_live_config = load_bilibili_live_config(config)
+        screenshot_config = load_screenshot_config(config)
+        blip_config = load_blip_image_captioning_large_config(config)
+        gpt_sovits_config = load_gpt_sovits_config(config)
+        tone_analysis_service_config = load_tone_analysis_service_config(config)
+        chatglm3_service_config = load_chatglm3_service_config(config)
+        zerolan_live_robot_config = load_zerolan_live_robot_config(config)
+        obs_config = load_obs_config(config)
 
-    bilibili_live_config = load_bilibili_live_config(config)
-    screenshot_config = load_screenshot_config(config)
-    blip_config = load_blip_image_captioning_large_config(config)
-    gpt_sovits_config = load_gpt_sovits_config(config)
-    tone_analysis_service_config = load_tone_analysis_service_config(config)
-    chatglm3_service_config = load_chatglm3_service_config(config)
-    zerolan_live_robot_config = load_zerolan_live_robot_config(config)
-    obs_config = load_obs_config(config)
+        # 初始化服务（赋初值 / 加载模型）
 
-    # 初始化服务（赋初值 / 加载模型）
-
-    assert bili_serv.init(*bilibili_live_config)
-    assert scrn_serv.init(*screenshot_config)
-    assert blip_serv.init(*blip_config)
-    assert gptsovits_serv.init(*gpt_sovits_config)
-    assert tone_serv.init(*tone_analysis_service_config)
-    assert chatglm3_serv.init(*chatglm3_service_config)
-    assert obs_serv.init(*obs_config)
-    assert init(*zerolan_live_robot_config)
+        assert bili_serv.init(*bilibili_live_config)
+        assert scrn_serv.init(*screenshot_config)
+        assert blip_serv.init(*blip_config)
+        assert gptsovits_serv.init(*gpt_sovits_config)
+        assert tone_serv.init(*tone_analysis_service_config)
+        assert chatglm3_serv.init(*chatglm3_service_config)
+        assert obs_serv.init(*obs_config)
+        assert init(*zerolan_live_robot_config)
+    except Exception:
+        logger.critical(f'💥 ZEROLAN LIVE ROBOT 初始化失败：因无法处理的异常而退出')
 
     asyncio.run(service_start())
