176c8b6cecde38510e62467bfb3253573d99824d
AkagawaTsurunaki <965145619@qq.com>
2024-03-18 21:03:30 +0800

    feat: 自动打怪功能

--- minecraft/py/guard.py
+++ minecraft/py/guard.py
@@ -1,5 +1,10 @@
-def attack_mobs(bot) :
-    # filter(lambda e: e.type == 'hostile' and e.position.distanceTo(bot.entity.position) < 8)
-    # if bot:
-    #     entity = bot.nearestEntity()
-    ...
+def attack_mobs(bot):
+    if bot:
+        bot_pos = bot.entity.position
+
+        def mob_filter(e):
+            return e.type == 'hostile' and e.position.distanceTo(bot_pos) < 8
+
+        entity = bot.nearestEntity(mob_filter)
+        if entity:
+            bot.pvp.attack(entity)
