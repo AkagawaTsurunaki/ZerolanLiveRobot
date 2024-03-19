9ab89bf8d8d302127f7ab7f9a89230593eb971ab
AkagawaTsurunaki <965145619@qq.com>
2024-03-19 13:32:45 +0800

    feat: 将游戏事件加入列表

--- minecraft/web.py
+++ minecraft/web.py
@@ -1,16 +1,22 @@
+from typing import List
+
 from flask import Flask, jsonify
 from flask import request
 from loguru import logger
 
-from minecraft.py.common import GameEvent
+from minecraft.common import GameEvent
 
 app = Flask(__name__)
+
+game_event_list: List[GameEvent] = []
 
 
 @app.route('/addevent', methods=['POST'])
 def handle_add_event():
     game_event = GameEvent(**request.json)
     logger.info(game_event)
+    if game_event:
+        game_event_list.append(game_event)
     return jsonify(game_event)
 
 
