74e463d99afc8e45eb9c368f376fdc72e468b454
AkagawaTsurunaki <965145619@qq.com>
2024-03-26 21:51:48 +0800

    feat: 支持命令行快速启动模型

--- api_run.py
+++ api_run.py
@@ -1,13 +1,21 @@
 import argparse
 
+import asr.service
 import blip_img_cap.service
+import chatglm3.service
 
 parser = argparse.ArgumentParser(description='启动分布式服务的工具')
 
-parser.add_argument('blip', type=bool, default=False)
-# parser.add_argument('blip', type=bool, default=
-# )
+parser.add_argument('--service', type=str, default=False)
 
 args = parser.parse_args()
-if args.blip:
-    blip_img_cap.service.start()
+
+startable_service_dict = {
+    'blip': blip_img_cap.service.start,
+    'chatglm3': chatglm3.service.start,
+    'asr': asr.service.start
+}
+
+if args.service:
+    start_func = startable_service_dict.get(args.service)
+    start_func()
