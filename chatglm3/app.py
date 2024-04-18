84964bcb7525e9a5e1075b86666f55e55ecdb6f4
AkagawaTsurunaki <965145619@qq.com>
2024-04-18 13:56:12 +0800

    refactor: chatglm3 API modified

--- chatglm3/app.py
+++ chatglm3/app.py
@@ -1,10 +1,14 @@
+from dataclasses import asdict
+
 from flask import Flask, jsonify, stream_with_context, Response, request
 from loguru import logger
 from transformers import AutoTokenizer, AutoModel
 
 import initzr
 from config.global_config import Chatglm3ServiceConfig
-from utils.datacls import LLMQuery
+
+from llm.pipeline import LLMPipeline
+from utils.datacls import LLMQuery, NewLLMResponse, Chat, NewLLMQuery
 
 TOKENIZER: AutoTokenizer
 MODEL: AutoModel
@@ -12,23 +16,27 @@ MODEL: AutoModel
 app = Flask(__name__)
 
 
-def _predict(query: str, history: list = None, top_p: float = 1., temperature: float = 1.,
-             return_past_key_values: bool = True) -> (str, list):
-    ret_response = ''
-    ret_history = None
-    past_key_values = None
+def _predict(llm_query: NewLLMQuery) -> (str, list):
+    query = llm_query.text
+    history = [{'role': chat.role, 'metadata': '', 'content': chat.content} for chat in llm_query.history]
+    response, history = MODEL.chat(TOKENIZER, query, history, top_p=1., temperature=1., past_key_values=None,
+                                   return_past_key_values=True)
+    history = [Chat(role=chat['role'], content=chat['content']) for chat in history]
+    llm_response = NewLLMResponse(response=response, history=history)
+    return llm_response
+    # ret_response = ''
+    # ret_history = None
+    # past_key_values = None
 
-    for response, history, past_key_values in MODEL.stream_chat(TOKENIZER,
-                                                                query,
-                                                                history=history if history else [],
-                                                                top_p=top_p,
-                                                                temperature=temperature,
-                                                                past_key_values=past_key_values,
-                                                                return_past_key_values=return_past_key_values):
-        ret_response = response
-        ret_history = history
-
-    return ret_response, ret_history
+    # for response, history, past_key_values in MODEL.stream_chat(TOKENIZER,
+    #                                                             query,
+    #                                                             history=history if history else [],
+    #                                                             top_p=1.,
+    #                                                             temperature=1.,
+    #                                                             past_key_values=past_key_values,
+    #                                                             return_past_key_values=True):
+    #     ret_response = response
+    #     ret_history = history
 
 
 def _stream_predict(query: str, history: list = None, top_p: float = 1., temperature: float = 1.,
@@ -47,15 +55,9 @@ def _stream_predict(query: str, history: list = None, top_p: float = 1., tempera
 @app.route('/chatglm3/predict', methods=['GET'])
 def handle_output():
     json_val = request.get_json()
-    llm_query = LLMQuery(**json_val)
-    response, history = _predict(query=llm_query.query, history=llm_query.history, temperature=llm_query.temperature,
-                                 top_p=llm_query.top_p,
-                                 return_past_key_values=True)
-    print(response)
-    return jsonify({
-        "response": response,
-        "history": history
-    })
+    llm_query = LLMPipeline.convert_query_from_json(json_val)
+    llm_response = _predict(llm_query)
+    return jsonify(asdict(llm_response))
 
 
 @app.route('/chatglm3/streampredict', methods=['GET'])
