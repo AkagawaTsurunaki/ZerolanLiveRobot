4edb684d0b20fe896f0b3de62adf1d94e7ff174d
AkagawaTsurunaki <965145619@qq.com>
2024-04-18 17:06:33 +0800

    perf: all threads will be started in a list

--- main.py
+++ main.py
@@ -20,35 +20,27 @@ if __name__ == '__main__':
         # 线程列表
         thread_list = []
 
-        # 启动弹幕监听服务
-        bili_thread = threading.Thread(target=bilibili.service.start)
-        thread_list.append(bili_thread)
-        bili_thread.start()
+        # 弹幕监听服务
+        thread_list.append(threading.Thread(target=bilibili.service.start))
 
-        # 启动播放器线程
-        audio_play_thread = threading.Thread(target=audio_player.service.start)
-        thread_list.append(audio_play_thread)
-        audio_play_thread.start()
+        # 播放器线程
+        thread_list.append(threading.Thread(target=audio_player.service.start))
 
-        # 启动 Minecraft 游戏事件监听线程
-        minecraft_thread = threading.Thread(target=minecraft.app.start)
-        thread_list.append(minecraft_thread)
-        minecraft_thread.start()
+        # Minecraft 游戏事件监听线程
+        thread_list.append(threading.Thread(target=minecraft.app.start))
 
-        # 启动 VAD 服务线程
-        vad_serv_thread = threading.Thread(target=vad.service.start)
-        thread_list.append(vad_serv_thread)
-        vad_serv_thread.start()
+        # VAD 服务线程
+        thread_list.append(threading.Thread(target=vad.service.start))
 
-        # 启动控制器
-        ctrl_thread = threading.Thread(target=controller.app.start)
-        thread_list.append(ctrl_thread)
-        ctrl_thread.start()
+        # ASR 线程
+        thread_list.append(threading.Thread(target=asr.service.start))
 
-        # 启动 ASR 线程
-        asr_thread = threading.Thread(target=asr.service.start)
-        thread_list.append(asr_thread)
-        asr_thread.start()
+        # 控制器线程
+        thread_list.append(threading.Thread(target=controller.app.start))
+
+        # 启动所有线程
+        for thread in thread_list:
+            thread.start()
 
         # 启动生命周期
         asyncio.run(service_start())
