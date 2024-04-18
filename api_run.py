596e32717ca8e5375e1354921d709c806fb0dfb2
AkagawaTsurunaki <965145619@qq.com>
2024-04-18 21:52:18 +0800

    fix: argument parsing error fixed.

--- api_run.py
+++ api_run.py
@@ -1,34 +1,37 @@
 import argparse
 
-import asr.app
-import blip_img_cap.app
-import llm.chatglm3.app
-import llm.qwen.app
-import llm.yi_6b.app
-
 parser = argparse.ArgumentParser(description='启动分布式服务的工具')
 
-parser.add_argument('--service', '-s', type=str, default=False)
-parser.add_argument('--model-path', '-mp', type=str, default="chatglm3")
-parser.add_argument('--quantize', '-q', type=int, default=4)
-parser.add_argument('--loading-mode', '-lm', type=str, default='auto')
-parser.add_argument('--host', '-h', type=str, default='127.0.0.1')
-parser.add_argument('--port', '-p', type=int, default=9881)
-parser.add_argument('--debug', '-d', type=str, default=False)
+parser.add_argument('--service', type=str, default=False)
+parser.add_argument('--model-path', type=str, default="chatglm3")
+parser.add_argument('--quantize', type=int, default=4)
+parser.add_argument('--loading-mode', type=str, default='auto')
+parser.add_argument('--host', type=str, default='127.0.0.1')
+parser.add_argument('--port', type=int, default=9881)
+parser.add_argument('--debug', type=bool, default=False)
 
-service, model_path, quantize, loading_mode, host, port, debug = parser.parse_args()
 args = parser.parse_args()
 
 startable_service_dict = {}
 
 if args.service:
-    if service == 'asr':
+    if args.service == 'asr':
+        import asr.app
+
         asr.app.start()
-    elif service == 'blip':
+    elif args.service == 'blip':
+        import blip_img_cap.app
+
         blip_img_cap.app.start()
-    elif service == 'chatglm3':
-        llm.chatglm3.app.start(model_path, quantize, host, port, debug)
-    elif service == 'Qwen/Qwen-7B-Chat':
-        llm.qwen.app.start(model_path, loading_mode, host, port, debug)
-    elif service == '01-ai/Yi':
-        llm.yi_6b.app.start(model_path, loading_mode, host, port, debug)
+    elif args.service == 'chatglm3':
+        import llm.chatglm3.app
+
+        llm.chatglm3.app.start(args.model_path, args.quantize, args.host, args.port, args.debug)
+    elif args.service == 'Qwen/Qwen-7B-Chat':
+        import llm.qwen.app
+
+        llm.qwen.app.start(args.model_path, args.loading_mode, args.host, args.port, args.debug)
+    elif args.service == '01-ai/Yi':
+        import llm.yi_6b.app
+
+        llm.yi_6b.app.start(args.model_path, args.loading_mode, args.host, args.port, args.debug)
