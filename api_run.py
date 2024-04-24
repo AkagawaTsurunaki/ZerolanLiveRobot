6637eacdcc17f5096a610ac0943b0722e5e75609
AkagawaTsurunaki <965145619@qq.com>
2024-04-24 21:51:03 +0800

    feat: Shisa supported

--- api_run.py
+++ api_run.py
@@ -1,9 +1,11 @@
 import argparse
 
+from utils.datacls import ServiceNameRegistry as SNR
+
 parser = argparse.ArgumentParser(description='启动分布式服务的工具')
 
 parser.add_argument('--service', type=str, default=False)
-parser.add_argument('--model-path', type=str, default="chatglm3")
+parser.add_argument('--model-path', type=str, default=SNR.CHATGLM3)
 parser.add_argument('--quantize', type=int, default=4)
 parser.add_argument('--loading-mode', type=str, default='auto')
 parser.add_argument('--host', type=str, default='127.0.0.1')
@@ -15,23 +17,27 @@ args = parser.parse_args()
 startable_service_dict = {}
 
 if args.service:
-    if args.service == 'asr':
+    if args.service == SNR.ASR:
         import asr.app
 
         asr.app.start()
-    elif args.service == 'blip':
+    elif args.service == SNR.BLIP:
         import blip_img_cap.app
 
         blip_img_cap.app.start()
-    elif args.service == 'chatglm3':
+    elif args.service == SNR.CHATGLM3:
         import llm.chatglm3.app
 
         llm.chatglm3.app.start(args.model_path, args.quantize, args.host, args.port, args.debug)
-    elif args.service == 'Qwen/Qwen-7B-Chat':
+    elif args.service == SNR.QWEN:
         import llm.qwen.app
 
         llm.qwen.app.start(args.model_path, args.loading_mode, args.host, args.port, args.debug)
-    elif args.service == '01-ai/Yi':
+    elif args.service == SNR.YI:
         import llm.yi_6b.app
 
         llm.yi_6b.app.start(args.model_path, args.loading_mode, args.host, args.port, args.debug)
+    elif args.service == SNR.SHISA:
+        import llm.shisa.app
+
+        llm.shisa.app.start(args.model_path, args.host, args.port, args.debug)
