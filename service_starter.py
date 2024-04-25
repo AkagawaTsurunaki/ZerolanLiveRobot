2c75cb05e22d87d355c1cb20c36709ff53e89a94
AkagawaTsurunaki <965145619@qq.com>
2024-04-25 23:08:34 +0800

    feat: Models will be loaded through script

--- service_starter.py
+++ service_starter.py
@@ -1,7 +1,10 @@
+import argparse
+
+from loguru import logger
+
 import livestream.bilibili.service
-from config import ASRConfig, ImageCaptioningConfig, LLMServiceConfig, TextToSpeechConfig
+from config import ASRConfig, ImageCaptioningConfig, LLMServiceConfig, TextToSpeechConfig, LiveStreamConfig
 from config import GLOBAL_CONFIG as G_CFG
-from config import LiveStreamConfig
 from utils.datacls import ServiceNameConst as SNR, PlatformConst
 
 
@@ -59,14 +62,34 @@ def live_stream_start(config: LiveStreamConfig):
 
 
 def start():
-    assert G_CFG.large_language_model.enable, f'LLM service must be enabled.'
-    start_llm(G_CFG.large_language_model)
+    processes = []
 
-    if G_CFG.auto_speech_recognition.enable:
-        start_asr(G_CFG.auto_speech_recognition)
+    return processes
 
-    if G_CFG.image_captioning.enable:
-        start_img_cap(G_CFG.image_captioning)
 
-    if G_CFG.text_to_speech.enable:
-        start_tts(G_CFG.text_to_speech)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--service', '-s', type=str)
+    args = parser.parse_args()
+    if 'asr' == args.service:
+        if G_CFG.auto_speech_recognition.enable:
+            start_asr(G_CFG.auto_speech_recognition)
+        else:
+            logger.warning('ASR service is not allowed to start: Check your configuration file.')
+    elif 'llm' == args.service:
+        if G_CFG.large_language_model.enable:
+            start_llm(G_CFG.large_language_model)
+        else:
+            logger.warning('LLM service is not allowed to start: Check your configuration file.')
+    elif 'ic' == args.service:
+        if G_CFG.image_captioning.enable:
+            start_img_cap(G_CFG.image_captioning)
+        else:
+            logger.warning('Image-captioning service is not allowed to start: Check your configuration file.')
+    elif 'tts' == args.service:
+        if G_CFG.text_to_speech.enable:
+            start_tts(G_CFG.text_to_speech)
+        else:
+            logger.warning('TTS service is not allowed to start: Check your configuration file.')
+    else:
+        raise NotImplementedError(f'{args.service} service is not supported.')
