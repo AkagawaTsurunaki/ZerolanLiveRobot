8d86d3f8d59ac4c14c764b1f54e5e1da9fc87062
AkagawaTsurunaki <965145619@qq.com>
2024-04-27 23:58:44 +0800

    refactor: service_starter.py

--- service_starter.py
+++ service_starter.py
@@ -3,51 +3,41 @@ import argparse
 from loguru import logger
 
 from common.abs_service import AbstractService
-from config import ASRConfig, ImageCaptioningConfig, TextToSpeechConfig, GlobalConfig
-from config import GLOBAL_CONFIG as G_CFG
 from common.datacls import ServiceNameConst as SNC, PlatformConst
+from config import GLOBAL_CONFIG as G_CFG
+from config import GlobalConfig
 
 
-def start_asr(config: ASRConfig):
-    model = config.models[0]
-    if SNC.PARAFORMER == model.model_name:
-        from asr.speech_paraformer.app import SpeechParaformerApp
-
-        SpeechParaformerApp(config).start()
+def start_asr(model_name: str):
+    if SNC.PARAFORMER == model_name:
+        import asr.speech_paraformer.app
+        asr.speech_paraformer.app.start()
 
 
-def start_img_cap(config: ImageCaptioningConfig):
-    model = config.models[0]
-    if SNC.BLIP == model.model_name:
-        from img_cap.blip.app import BlipApp
-
-        BlipApp(config).start()
+def start_img_cap(model_name: str):
+    if SNC.BLIP == model_name:
+        import img_cap.blip.app
+        img_cap.blip.app.start()
 
 
-def start_llm(config: GlobalConfig):
-    model = config.large_language_model.models[0]
-    if SNC.CHATGLM3 == model.model_name:
-        from llm.chatglm3.app import ChatGLM3App
-
-        ChatGLM3App(config).start()
-    elif SNC.QWEN == model.model_name:
-        from llm.qwen.app import QwenApp
-
-        QwenApp(config).start()
-    elif SNC.YI == model.model_name:
-        from llm.yi_6b.app import YiApp
-
-        YiApp(config).start()
-    elif SNC.SHISA == model.model_name:
-        from llm.shisa.app import ShisaApp
-
-        ShisaApp(config).start()
+def start_llm(model_name: str):
+    if SNC.CHATGLM3 == model_name:
+        import llm.chatglm3.app
+        llm.chatglm3.app.start()
+    elif SNC.QWEN == model_name:
+        import llm.qwen.app
+        llm.qwen.app.start()
+    elif SNC.YI == model_name:
+        import llm.yi_6b.app
+        llm.yi_6b.app.start()
+    elif SNC.SHISA == model_name:
+        import llm.shisa.app
+        llm.shisa.app.start()
 
 
-def start_tts(config: TextToSpeechConfig):
-    model = config.models[0]
-    if SNC.GPT_SOVITS == model.model_name:
-        pass
+def start_tts(model_name: str):
+    if SNC.GPT_SOVITS == model_name:
+        logger.warning('No need to start app...')
 
 
 def get_live_stream_service(config: GlobalConfig) -> AbstractService:
@@ -63,22 +53,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if 'asr' == args.service:
         if G_CFG.auto_speech_recognition.enable:
-            start_asr(G_CFG.auto_speech_recognition)
+            start_asr(G_CFG.auto_speech_recognition.models[0].model_name)
         else:
             logger.warning('ASR service is not allowed to start: Check your configuration file.')
     elif 'llm' == args.service:
         if G_CFG.large_language_model.enable:
-            start_llm(G_CFG.large_language_model)
+            start_llm(G_CFG.large_language_model.models[0].model_name)
         else:
             logger.warning('LLM service is not allowed to start: Check your configuration file.')
     elif 'ic' == args.service:
         if G_CFG.image_captioning.enable:
-            start_img_cap(G_CFG.image_captioning)
+            start_img_cap(G_CFG.image_captioning.models[0].model_name)
         else:
             logger.warning('Image-captioning service is not allowed to start: Check your configuration file.')
     elif 'tts' == args.service:
         if G_CFG.text_to_speech.enable:
-            start_tts(G_CFG.text_to_speech)
+            start_tts(G_CFG.text_to_speech.models[0].model_name)
         else:
             logger.warning('TTS service is not allowed to start: Check your configuration file.')
     else:
