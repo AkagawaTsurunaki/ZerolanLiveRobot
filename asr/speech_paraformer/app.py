f215af120fd6ccc62c4259ba8616a6e946ed5fb3
AkagawaTsurunaki <965145619@qq.com>
2024-04-27 23:26:04 +0800

    reverted: No usage of class.

--- asr/speech_paraformer/app.py
+++ asr/speech_paraformer/app.py
@@ -4,43 +4,51 @@ from flask import Flask, request, jsonify
 from funasr import AutoModel
 from loguru import logger
 
-from common.abs_app import AbstractApp
-from config import ASRConfig
+from config import GlobalConfig
 
-app = Flask(__name__)
+# Global attributes
+_app = Flask(__name__)  # Flask application instance
+
+# Initialize global variables
+_host: str  # Host address for the Flask application
+_port: int  # Port number for the Flask application
+_debug: bool  # Debug mode flag for the Flask application
+_model: any  # ASR model for recognize speeches
 
 
-class SpeechParaformerApp(AbstractApp):
+def init(cfg: GlobalConfig):
+    global _host, _port, _debug, _model
+    asr_cfg = cfg.auto_speech_recognition
+    model_path = asr_cfg.models[0].model_path
+    version = asr_cfg.models[0].version
+    _host = asr_cfg.host
+    _port = asr_cfg.port
+    _debug = asr_cfg.debug
+    logger.info('👂️ Auto speech recognition service initializing...')
+    _model = AutoModel(model=model_path, model_revision=version)
+    logger.info(f'👂️ Auto speech recognition model {model_path} loaded.')
 
-    def __init__(self, cfg: ASRConfig):
-        super().__init__()
-        self._model_path = cfg.models[0].model_path
-        self._version = cfg.models[0].version
-        self._host = cfg.host
-        self._port = cfg.port
-        self._debug = cfg.debug
-        logger.info('👂️ Auto speech recognition service initializing...')
-        self._model = AutoModel(model=self._model_path, model_revision=self._version)
-        logger.info(f'👂️ Auto speech recognition model {self._model_path} loaded.')
 
-    def start(self):
-        app.run(host=self._host, port=self._port, debug=self._debug)
+def start():
+    _app.run(host=_host, port=_port, debug=_debug)
 
-    @app.route('/asr/predict', methods=['GET', 'POST'])
-    def handle_predict(self):
-        req = request.json
-        wav_path = req.get('wav_path', None)
-        assert os.path.exists(wav_path), f'Can not find wav file path: "{wav_path}"'
-        transcript = self._predict(wav_path)
-        return jsonify({'transcript': transcript})
 
-    def _predict(self, wav_path) -> str | None:
-        try:
-            res = self._model.generate(input=wav_path,
-                                       batch_size_s=300,
-                                       hotword='魔搭')
-            res = res[0]['text']
-            return res
-        except Exception as e:
-            logger.exception(e)
-            return None
+def _predict(wav_path) -> str | None:
+    try:
+        res = _model.generate(input=wav_path,
+                              batch_size_s=300,
+                              hotword='魔搭')
+        res = res[0]['text']
+        return res
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+
+@_app.route('/asr/predict', methods=['GET', 'POST'])
+def handle_predict():
+    req = request.json
+    wav_path = req.get('wav_path', None)
+    assert os.path.exists(wav_path), f'Can not find wav file path: "{wav_path}"'
+    transcript = _predict(wav_path)
+    return jsonify({'transcript': transcript})
