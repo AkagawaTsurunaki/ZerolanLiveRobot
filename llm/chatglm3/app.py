f00b9b5e8d04aa74b2db0c32607cf87e45b23b62
AkagawaTsurunaki <965145619@qq.com>
2024-04-27 22:25:52 +0800

    revert: No usage of class.

--- llm/chatglm3/app.py
+++ llm/chatglm3/app.py
@@ -4,78 +4,78 @@ from flask import Flask, jsonify, stream_with_context, Response, request
 from loguru import logger
 from transformers import AutoTokenizer, AutoModel
 
-from common.abs_app import AbstractApp
+from common.datacls import LLMResponse, Chat, LLMQuery
 from config import GlobalConfig
 from llm.pipeline import LLMPipeline
-from common.datacls import LLMResponse, Chat, LLMQuery
 
+_tokenizer: AutoTokenizer
+_model: AutoModel
 app = Flask(__name__)
+_host: str
+_port: int
+_debug: bool
 
 
-class ChatGLM3App(AbstractApp):
+def _predict(llm_query: LLMQuery) -> LLMResponse:
+    query = llm_query.text
+    history = [{'role': chat.role, 'metadata': '', 'content': chat.content} for chat in llm_query.history]
+    response, history = _model.chat(_tokenizer, query, history, top_p=1., temperature=1., past_key_values=None)
+    logger.info(response)
+    history = [Chat(role=chat['role'], content=chat['content']) for chat in history]
+    llm_response = LLMResponse(response=response, history=history)
+    return llm_response
 
-    def __init__(self, cfg: GlobalConfig):
-        super().__init__()
-        chatglm_cfg = cfg.large_language_model.models[0]
-        self._model_path = chatglm_cfg.model_path
-        self._model_name = chatglm_cfg.model_name
-        self._quantize = chatglm_cfg.quantize
-        self.host = cfg.large_language_model.host
-        self.host.port = cfg.large_language_model.port
-        self.debug = cfg.large_language_model.debug
 
-        self._tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained(self._model_path, trust_remote_code=True)
+def _stream_predict(llm_query: LLMQuery):
+    query = llm_query.text
+    history = [{'role': chat.role, 'metadata': '', 'content': chat.content} for chat in llm_query.history]
+    yield _model.stream_chat(_tokenizer, query, history=history, top_p=1., temperature=1.,
+                             past_key_values=None,
+                             return_past_key_values=True)
 
-        if self._quantize:
-            self._model: AutoModel = AutoModel.from_pretrained(self._model_path, trust_remote_code=True) \
-                .quantize(self._quantize) \
-                .to('cuda').eval()
-        else:
-            self._model: AutoModel = AutoModel.from_pretrained(self._model_path, trust_remote_code=True) \
-                .to('cuda').eval()
-        logger.info(f'ChatGLM3 model loaded.')
 
-    def start(self):
-        app.run(host=self.host, port=self.port, debug=self.debug)
+@app.route(f'/llm/predict', methods=['GET', 'POST'])
+def handle_predict():
+    json_val = request.get_json()
+    llm_query = LLMPipeline.parse_query_from_json(json_val)
+    llm_response = _predict(llm_query)
+    return jsonify(asdict(llm_response))
 
-    def _predict(self, llm_query: LLMQuery) -> LLMResponse:
-        query = llm_query.text
-        history = [{'role': chat.role, 'metadata': '', 'content': chat.content} for chat in llm_query.history]
-        response, history = self._model.chat(self._tokenizer, query, history, top_p=1., temperature=1.,
-                                             past_key_values=None)
-        logger.info(response)
-        history = [Chat(role=chat['role'], content=chat['content']) for chat in history]
-        llm_response = LLMResponse(response=response, history=history)
-        return llm_response
 
-    def _stream_predict(self, llm_query: LLMQuery):
-        query = llm_query.text
-        history = [{'role': chat.role, 'metadata': '', 'content': chat.content} for chat in llm_query.history]
-        yield self._model.stream_chat(self._tokenizer, query, history=history, top_p=1., temperature=1.,
-                                      past_key_values=None,
-                                      return_past_key_values=True)
+@app.route(f'/llm/stream-predict', methods=['GET', 'POST'])
+def handle_stream_predict():
+    json_val = request.get_json()
+    print(json_val)
+    llm_query = LLMPipeline.parse_query_from_json(json_val)
 
-    @app.route(f'/llm/predict', methods=['GET', 'POST'])
-    def handle_predict(self):
-        json_val = request.get_json()
-        llm_query = LLMPipeline.parse_query_from_json(json_val)
-        llm_response = self._predict(llm_query)
-        return jsonify(asdict(llm_response))
+    def generate_output(q: LLMQuery):
+        with app.app_context():
+            for response, history, past_key_values in next(_stream_predict(q)):
+                history = [Chat(role=chat['role'], content=chat['content']) for chat in history]
+                llm_response = LLMResponse(
+                    response=response,
+                    history=history
+                )
+                yield jsonify(llm_response).data + b'\n'
 
-    @app.route(f'/llm/stream-predict', methods=['GET', 'POST'])
-    def handle_stream_predict(self):
-        json_val = request.get_json()
-        print(json_val)
-        llm_query = LLMPipeline.parse_query_from_json(json_val)
+    return Response(stream_with_context(generate_output(llm_query)), content_type='application/json')
 
-        def generate_output(q: LLMQuery):
-            with app.app_context():
-                for response, history, past_key_values in next(self._stream_predict(q)):
-                    history = [Chat(role=chat['role'], content=chat['content']) for chat in history]
-                    llm_response = LLMResponse(
-                        response=response,
-                        history=history
-                    )
-                    yield jsonify(llm_response).data + b'\n'
 
-        return Response(stream_with_context(generate_output(llm_query)), content_type='application/json')
+def init(cfg: GlobalConfig):
+    global _host, _port, _debug, _model, _tokenizer
+    _host = cfg.large_language_model.host
+    _port = cfg.large_language_model.port
+    _debug = cfg.large_language_model.debug
+    model_path = cfg.large_language_model.models[0].model_path
+    quantize = cfg.large_language_model.models[0].quantize
+    _tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    if quantize:
+        _model = AutoModel.from_pretrained(model_path, trust_remote_code=True).quantize(quantize).to(
+            'cuda').eval()
+    else:
+        _model = AutoModel.from_pretrained(model_path, trust_remote_code=True).to('cuda').eval()
+    logger.info(f'💭 ChatGLM3 model loaded.')
+
+
+def start():
+    app.run(host=_host, port=_port, debug=_debug)
