5eed8882161541baf7fec1d73e3418eae4c1325f
AkagawaTsurunaki <965145619@qq.com>
2024-05-12 23:22:28 +0800

    feat: 使用装饰器封装日志逻辑

--- common/exc.py
+++ common/exc.py
@@ -1,3 +1,11 @@
+from functools import wraps
+from time import time
+
+from loguru import logger
+
+from config import GLOBAL_CONFIG as G_CFG
+
+
 class InitError(Exception):
     def __init__(self, message):
         super().__init__(self)
@@ -5,3 +13,21 @@ class InitError(Exception):
 
     def __str__(self):
         return self.message
+
+
+def model_loading_log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        model_name = G_CFG.large_language_model.models[0].model_name
+        try:
+            logger.info(f'💭 模型 {model_name} 正在加载……')
+            time_start = time()
+            ret = func(*args, **kwargs)
+            time_end = time()
+            time_used = "{:.2f}".format(time_end - time_start)
+            logger.info(f'💭 模型 {model_name} 加载完毕，用时 {time_used} 秒。')
+            return ret
+        except AssertionError:
+            logger.critical(f'❌️ 模型 {model_name} 加载失败。')
+
+    return wrapper
