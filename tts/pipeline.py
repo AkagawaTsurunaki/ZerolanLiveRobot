e7f927b370263aad2e8ab299649193ae1d17f842
AkagawaTsurunaki <965145619@qq.com>
2024-05-13 16:43:50 +0800

    refactor: 修改了 GPT-SoVITS 的推理逻辑

--- tts/pipeline.py
+++ tts/pipeline.py
@@ -3,10 +3,10 @@ from http import HTTPStatus
 
 import requests
 
-from common.abs_pipeline import AbstractPipeline
-from config import GlobalConfig
-from common.datacls import ModelNameConst as SNC, TTSQuery, TTSResponse
 from common import util
+from common.abs_pipeline import AbstractPipeline
+from common.datacls import ModelNameConst as SNC, TTSQuery, TTSResponse
+from config import GlobalConfig
 
 
 class TTSPipeline(AbstractPipeline):
@@ -15,18 +15,17 @@ class TTSPipeline(AbstractPipeline):
         host, port = cfg.text_to_speech.host, cfg.text_to_speech.port
 
         self.predict_url = util.urljoin(host, port, '/tts/predict')
-        self.predict_url_4_gpt_sovits = util.urljoin(host, port)
         self.model_name = cfg.text_to_speech.models[0].model_name
 
     def predict(self, query: TTSQuery) -> TTSResponse | None:
         if SNC.GPT_SOVITS == self.model_name:
-            return self._gpt_sovits_predict(query)
+            return self._predict(query)
         else:
-            return super().predict(query)
+            raise NotImplementedError("尚未实现")
 
-    def _gpt_sovits_predict(self, tts_query: TTSQuery) -> TTSResponse | None:
+    def _predict(self, tts_query: TTSQuery) -> TTSResponse | None:
         query = asdict(tts_query)
-        response = requests.post(self.predict_url_4_gpt_sovits, json=query)
+        response = requests.post(self.predict_url, json=query)
         if response.status_code == HTTPStatus.OK:
             return TTSResponse(wave_data=response.content)
         return None
