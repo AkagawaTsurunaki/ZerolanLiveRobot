c9845d1862a531cae85ae402d8136203b124a5c2
AkagawaTsurunaki <965145619@qq.com>
2024-05-13 16:42:54 +0800

    feat: 支持写入文件名

--- zio/writer.py
+++ zio/writer.py
@@ -11,10 +11,10 @@ class Writer:
         with open(file=self.path, mode='w+', encoding=self.encode) as f:
             f.write(content)
 
-    def write_wav(self, data) -> str:
-        ran_file_name = uuid.uuid4()
-        tmp_wav_file_path = os.path.join(self.path, f'{ran_file_name}.wav')
+    def write_wav(self, data: bytes, filename: str | None = None) -> str:
+        filename = uuid.uuid4() if filename is None else filename
+        tmp_wav_file_path = os.path.join(self.path, f'{filename}.wav')
+        tmp_wav_file_path = tmp_wav_file_path.replace("/", "\\")
         with open(file=tmp_wav_file_path, mode='wb') as wav_file:
             wav_file.write(data)
-        tmp_wav_file_path = tmp_wav_file_path.replace("/", "\\")
         return tmp_wav_file_path
