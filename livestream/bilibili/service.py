dca2e6aaa58eacb14daac9847470d99ca76355d8
AkagawaTsurunaki <965145619@qq.com>
2024-05-14 12:02:04 +0800

    fix: 修复了读取弹幕异常的问题

--- livestream/bilibili/service.py
+++ livestream/bilibili/service.py
@@ -1,4 +1,4 @@
-import copy
+import random
 import threading
 from typing import List
 
@@ -95,5 +95,23 @@ def status() -> BilibiliServiceStatus:
         return BilibiliServiceStatus.STOP
 
 
-def all_danmakus():
-    return copy.deepcopy(_danmaku_list)
+def select_latest_longest(k: int) -> Danmaku:
+    # 按照某种策略拾取弹幕
+    # 按照当前时间戳最近的k条中随机挑选msg字段字符串最长的一条（若都相同，则随机）
+
+    # 选择出未读过的弹幕
+    unread_danmaku_list = [danmaku for danmaku in _danmaku_list if not danmaku.is_read]
+
+    # 如果弹幕数小于k
+    if len(unread_danmaku_list) < k:
+        selected_danmaku = max(unread_danmaku_list, key=lambda danmaku: len(danmaku.msg), default=None)
+    # 如果弹幕数大于k
+    else:
+        recent_danmakus = sorted(unread_danmaku_list, key=lambda danmaku: danmaku.ts, reverse=True)[:k]
+        max_length = max(len(danmaku.msg) for danmaku in recent_danmakus)
+        longest_danmakus = [danmaku for danmaku in recent_danmakus if len(danmaku.msg) == max_length]
+        selected_danmaku = random.choice(longest_danmakus) if longest_danmakus else None
+    # 将选择的弹幕标记为已读
+    if selected_danmaku:
+        selected_danmaku.is_read = True
+    return selected_danmaku
--- livestream/pipeline.py
+++ livestream/pipeline.py
@@ -23,29 +23,8 @@ class LiveStreamPipeline:
     def read_danmaku_latest_longest(self, k: int = 3) -> Danmaku | None:
         if self.platform == PlatformConst.BILIBILI:
             import livestream.bilibili.service
-            danmaku_list = livestream.bilibili.service.all_danmakus()
-            return self._select_latest_longest(danmaku_list, k)
+            return livestream.bilibili.service.select_latest_longest(k=3)
         else:
             raise NotImplementedError(f'Platform {self.platform} is not supported.')
 
-    @staticmethod
-    def _select_latest_longest(danmaku_list: list[Danmaku], k: int) -> Danmaku:
-        # 按照某种策略拾取弹幕
-        # 按照当前时间戳最近的k条中随机挑选msg字段字符串最长的一条（若都相同，则随机）
 
-        # 选择出未读过的弹幕
-        unread_danmaku_list = [danmaku for danmaku in danmaku_list if not danmaku.is_read]
-
-        # 如果弹幕数小于k
-        if len(unread_danmaku_list) < k:
-            selected_danmaku = max(unread_danmaku_list, key=lambda danmaku: len(danmaku.msg), default=None)
-        # 如果弹幕数大于k
-        else:
-            recent_danmakus = sorted(unread_danmaku_list, key=lambda danmaku: danmaku.ts, reverse=True)[:k]
-            max_length = max(len(danmaku.msg) for danmaku in recent_danmakus)
-            longest_danmakus = [danmaku for danmaku in recent_danmakus if len(danmaku.msg) == max_length]
-            selected_danmaku = random.choice(longest_danmakus) if longest_danmakus else None
-        # 将选择的弹幕标记为已读
-        if selected_danmaku:
-            selected_danmaku.is_read = True
-        return selected_danmaku
