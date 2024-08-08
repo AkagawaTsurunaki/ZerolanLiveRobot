9fc8267c007c611b26ee46535d62e1e2ee2b94e8
AkagawaTsurunaki <965145619@qq.com>
2024-08-08 11:01:10 +0800

    refactor: GPTSOVITS 改用环境变量进行启动配置

--- scripts/win_tts.bat
+++ scripts/win_tts.bat
@@ -1,18 +1,8 @@
 @echo off
 call activate GPTSoVits
 
-cd ../../
-
-rem 注意修改您自己的 GPT-SoVITS 路径
-set project_dir="../GPT-SoVITS"
-
-cd /d %project_dir% || (
-    echo Cannot find the project folder %project_dir%.
-    echo To solve this problem, you may try:
-    echo 1. Check if the directory exists.
-    echo 2. Download GPT-SoVITS from https://github.com/AkagawaTsurunaki/GPT-SoVITS
-    pause
-    exit /b 1
-)
+cd %GPT_SOVITS_PROJECT_DIR%
 
 python api.py -p 11006
+
+pause
