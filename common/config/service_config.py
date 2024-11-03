07595784c0f6de2787fb29cd6ab012c3f5fb34f5
AkagawaTsurunaki <965145619@qq.com>
2024-11-03 21:30:01 +0800

    refactor: Remove some configurations

--- common/config/service_config.py
+++ common/config/service_config.py
@@ -4,7 +4,6 @@ from typing import Literal
 from dataclasses_json import dataclass_json
 
 from common.config.abs_config import AbstractConfigLoader
-from common.register.model_register import ICModels, OCRModels, VidCapModels, TTSModels
 from common.utils.file_util import read_yaml, spath
 
 
@@ -12,54 +11,6 @@ from common.utils.file_util import read_yaml, spath
 class ControllerConfig:
     host: str = "127.0.0.1"
     port: int = 11000
-
-
-@dataclass
-class ImgCapServiceConfig:
-    # 是否启用本服务
-    enable: bool = True
-    # 服务地址
-    host: str = '127.0.0.1'
-    # 服务端口
-    port: int = 11003
-    # 模型 ID
-    model_id: str = ICModels.BLIP_IMG_CAP_LARGE.id
-
-
-@dataclass
-class OCRServiceConfig:
-    # 是否启用本服务
-    enable: bool = True
-    # 服务地址
-    host: str = '127.0.0.1'
-    # 服务端口
-    port: int = 11004
-    # 模型 ID
-    model_id: str = OCRModels.PADDLE_OCR.id
-
-
-@dataclass
-class VidCapServiceConfig:
-    # 是否启用本服务
-    enable: bool = True
-    # 服务地址
-    host: str = '127.0.0.1'
-    # 服务端口
-    port: int = 11005
-    # 模型 ID
-    model_id: str = VidCapModels.HITEA_BASE.id
-
-
-@dataclass
-class TTSServiceConfig:
-    # 是否启用本服务
-    enable: bool = True
-    # 服务地址
-    host: str = '127.0.0.1'
-    # 服务端口
-    port: int = 11006
-    # 模型 ID
-    model_id: str = TTSModels.GPT_SOVITS.id
 
 
 @dataclass_json
@@ -91,13 +42,7 @@ class GameServiceConfig:
 
 
 class ServiceConfig:
-    # asr_config: ASRServiceConfig | None = None
-    # llm_config: LLMServiceConfig | None = None
-    ocr_config: OCRServiceConfig | None = None
-    imgcap_config: ImgCapServiceConfig | None = None
     controller_config: ControllerConfig | None = None
-    vidcap_config: VidCapServiceConfig | None = None
-    tts_config: TTSServiceConfig | None = None
     live_stream_config: LiveStreamServiceConfig | None = None
     game_config: GameServiceConfig | None = None
 
@@ -106,13 +51,7 @@ class Loader(AbstractConfigLoader):
     @staticmethod
     def load_config():
         config_data = read_yaml(spath("resources/config/services_config.yaml"))
-        # ServiceConfig.asr_config = ASRServiceConfig(**config_data.get('ASRServiceConfig', {}))
-        # ServiceConfig.llm_config = LLMServiceConfig(**config_data.get('LLMServiceConfig', {}))
-        ServiceConfig.imgcap_config = ImgCapServiceConfig(**config_data.get('ImgCapServiceConfig', {}))
-        ServiceConfig.ocr_config = OCRServiceConfig(**config_data.get('OCRServiceConfig', {}))
-        ServiceConfig.controller_config = ControllerConfig(**config_data.get('ControllerConfig', {}))
-        ServiceConfig.vidcap_config = VidCapServiceConfig(**config_data.get('VidCapServiceConfig', {}))
-        ServiceConfig.tts_config = TTSServiceConfig(**config_data.get('TTSServiceConfig', {}))
+
         ServiceConfig.live_stream_config = LiveStreamServiceConfig.from_dict(  # type: ignore
             config_data.get(LiveStreamServiceConfig.__name__, {})
         )
