7ccb5b6dced9370548694e9424dd8ea9abf0ff90
AkagawaTsurunaki <965145619@qq.com>
2024-11-03 21:43:46 +0800

    refactor: Remove unused files

--- common/exception/unsafe_exception.py
+++
@@ -1,9 +0,0 @@
-from loguru import logger
-
-
-class UnsafeOperationException(Exception):
-    def __init__(self, msg: str):
-        super().__init__(msg)
-
-    def toast(self, msg):
-        logger.error("🛡️ 安全策略阻止此操作\n" + msg, block=True)
