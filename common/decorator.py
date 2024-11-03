187aad4433cedda2608f14b3784d8639a60bf4f3
AkagawaTsurunaki <965145619@qq.com>
2024-11-03 21:31:13 +0800

    refactor: Remove log_model_loading

--- common/decorator.py
+++ common/decorator.py
@@ -5,27 +5,6 @@ from typing import Callable
 from loguru import logger
 
 
-def log_model_loading(model_info):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                logger.info(f'模型 {model_info.id} 正在加载……')
-                time_start = time()
-                ret = func(*args, **kwargs)
-                time_end = time()
-                time_used = "{:.2f}".format(time_end - time_start)
-                logger.info(f'模型 {model_info.id} 加载完毕，用时 {time_used} 秒。')
-                return ret
-            except AssertionError as e:
-                logger.exception(e)
-                logger.critical(f'模型 {model_info.id} 加载失败。')
-
-        return wrapper
-
-    return decorator
-
-
 def log_run_time(log: Callable[[str], str] = None):
     def decorator(func):
         @wraps(func)
