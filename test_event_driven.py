13e17e219d1a04b3afcf1ab4f6b22719a71c72e6
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-14 11:03:30 +0800

    feat: Load prompts from TTSPromptManager

--- test_event_driven.py
+++ test_event_driven.py
@@ -1,5 +1,4 @@
 import asyncio
-from copy import deepcopy
 from dataclasses import dataclass
 
 from dataclasses_json import dataclass_json
@@ -10,6 +9,7 @@ from zerolan.data.data.tts import TTSQuery, TTSPrediction
 
 from common.eventemitter import EventEmitter
 from events.vad_event import VadEventEmitter
+from manager.tts_prompt_manager import TTSPromptManager
 from pipeline.asr import ASRPipeline
 from pipeline.llm import LLMPipeline
 from pipeline.tts import TTSPipeline
@@ -22,15 +22,6 @@ class GPT_SoVITS_TTS_Query(TTSQuery):
     cut_punc: str = "，。"
 
 
-default_tts_query = GPT_SoVITS_TTS_Query(
-    text="",
-    text_language="zh",
-    refer_wav_path=R"D:\AkagawaTsurunaki\WorkSpace\PythonProjects\ZerolanLiveRobot\resources\static\audio\momoi\[zh][正常]喜欢游戏的人和擅长游戏的人有很多不一样的地方，老师属于哪一种呢？.wav",
-    prompt_text="喜欢游戏的人和擅长游戏的人有很多不一样的地方，老师属于哪一种呢？",
-    prompt_language="zh",
-    cut_punc="，。")
-
-
 class ZerolanLiveRobot:
     def __init__(self):
         self.emitter = EventEmitter()
@@ -39,6 +30,8 @@ class ZerolanLiveRobot:
         self.tts = TTSPipeline()
 
         self.speaker = Speaker()
+
+        self.tts_prompts_manager = TTSPromptManager()
 
         self.history = []
 
@@ -74,15 +67,22 @@ class ZerolanLiveRobot:
         @self.emitter.on("asr")
         async def command_handler(prediction: ASRModelPrediction):
             if "关机" in prediction.transcript:
+                self.speaker.playsound(
+                    R"D:\AkagawaTsurunaki\WorkSpace\PythonProjects\ZerolanLiveRobot\resources\static\sound\exit.wav")
                 exit()
-
 
     def on_llm(self):
         @self.emitter.on("llm")
         async def llm_query_handler(prediction: LLMPrediction):
             logger.info("LLM: " + prediction.response)
-            query = deepcopy(default_tts_query)
-            query.text = prediction.response
+            tts_prompt = self.tts_prompts_manager.default_tts_prompt
+            query = TTSQuery(
+                text=prediction.response,
+                text_language="zh",
+                refer_wav_path=tts_prompt.audio_path,
+                prompt_text=tts_prompt.prompt_text,
+                prompt_language=tts_prompt.lang,
+            )
             for prediction in self.tts.stream_predict(query):
                 await self.emitter.emit("tts", prediction)
 
@@ -94,6 +94,7 @@ class ZerolanLiveRobot:
     def on_open(self):
         # @self.emitter.on("cnm")
         ...
+
 
 if __name__ == '__main__':
     bot = ZerolanLiveRobot()
