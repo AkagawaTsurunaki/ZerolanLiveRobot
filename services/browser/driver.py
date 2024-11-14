feaec757169b17d9bc4eecefd0a1aeaeb0385dd8
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-14 18:25:04 +0800

    refactor: DriverInitializer need config to create driver

--- services/browser/driver.py
+++ services/browser/driver.py
@@ -1,43 +1,41 @@
 import getpass
 import os
-from typing import Literal
+from xmlrpc.client import Error
 
 from loguru import logger
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver
 from selenium.webdriver.firefox.webdriver import WebDriver as FireFoxWebDriver
 
-
+from common.config import BrowserConfig
 from common.utils import file_util
 
 
 class DriverInitializer:
 
-    def __init__(self, browser: Literal["chrome", "firefox"]) -> None:
-        self._browser = browser
+    def __init__(self, config: BrowserConfig) -> None:
+        self._browser = config.driver
+        self._profile_dir = config.profile_dir
         self._driver = None
 
-    @staticmethod
-    def get_firefox_profile_dir() -> str:
+    def find_firefox_profile_dir(self):
 
-        # 从配置文件中读取
-        profile_dir: str | None = AutoConfigLoader.config.get("Tasks.Browser.Selenium.Firefox.ProfileDir", None)
-
-        if profile_dir is None or os.path.exists(profile_dir):
-            # 配置文件中找不到自动获取
-            logger.info("正在自动寻找 FireFox Profiles 目录")
+        if self._profile_dir is None or os.path.exists(self._profile_dir):
+            # Auto search if not set in the config
+            logger.info("Searching for FireFox Profiles...")
             username = getpass.getuser()
-            logger.debug(f"当前系统用户：{username}")
-            default_profile_dir = f"C:/Users/{username}/AppData/Roaming/Mozilla/Firefox/Profiles"
-            profile_dir = file_util.find_dir(default_profile_dir, ".default-release")
-            assert profile_dir is not None, f"找不到 FireFox Profiles 目录，请检查后重试。"
+            logger.debug(f"Current user: {username}")
+            if os.name == 'nt':
+                default_profile_dir = f"C:/Users/{username}/AppData/Roaming/Mozilla/Firefox/Profiles"
+                self._profile_dir = file_util.find_dir(default_profile_dir, ".default-release")
 
-        logger.info(f"找到可能的 FireFox Profiles 目录: {profile_dir}")
-        return profile_dir
+            if self._profile_dir is None:
+                raise Error("Can not find FireFox Profiles. Please set it in your config manually.")
+            else:
+                logger.info(f"Probable FireFox Profiles directory: {self._profile_dir}")
 
     def load_firefox_driver(self):
-        profile_dir: str = DriverInitializer.get_firefox_profile_dir()
-        profile = webdriver.FirefoxProfile(profile_dir)
+        profile = webdriver.FirefoxProfile(self._profile_dir)
         options = webdriver.FirefoxOptions()
         options.profile = profile
         driver = webdriver.Firefox(options=options)
