7c008d624f5c439c2b93572d9dc63537e01ddbc8
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-14 18:23:32 +0800

    feat: Implement some commands

--- test_event_driven.py
+++ test_event_driven.py
@@ -8,6 +8,7 @@ from zerolan.data.data.llm import LLMQuery, LLMPrediction
 from zerolan.data.data.tts import TTSQuery, TTSPrediction
 
 from common.config import get_config
+from common.decorator import withsound
 from common.enum import SystemSoundEnum
 from common.eventemitter import EventEmitter
 from events.vad_event import VadEventEmitter
@@ -17,6 +18,7 @@ from manager.tts_prompt_manager import TTSPromptManager
 from pipeline.asr import ASRPipeline
 from pipeline.llm import LLMPipeline
 from pipeline.tts import TTSPipeline
+from services.browser.browser import Browser
 from services.device.speaker import Speaker
 
 config = get_config()
@@ -31,6 +33,8 @@ class GPT_SoVITS_TTS_Query(TTSQuery):
 class ZerolanLiveRobot:
     def __init__(self):
         self.emitter = EventEmitter()
+        self.vad_emitter = VadEventEmitter(self.emitter)
+
         self.asr = ASRPipeline(config.pipeline.asr)
         self.llm = LLMPipeline(config.pipeline.llm)
         self.tts = TTSPipeline(config.pipeline.tts)
@@ -41,6 +45,7 @@ class ZerolanLiveRobot:
         self.chat_manager = LLMPromptManager(config.character.chat)
         self.temp_data_manager = TempDataManager()
 
+    @withsound(SystemSoundEnum.start)
     async def start(self):
         task = asyncio.create_task(self.start_emitters())
         self.on_voice()
@@ -50,8 +55,7 @@ class ZerolanLiveRobot:
         await task
 
     async def start_emitters(self):
-        vad_emitter = VadEventEmitter(self.emitter)
-        task = asyncio.create_task(vad_emitter.start())
+        task = asyncio.create_task(self.vad_emitter.start())
         await task
 
     def on_voice(self):
@@ -77,18 +81,35 @@ class ZerolanLiveRobot:
             if "关机" in prediction.transcript:
                 self.speaker.play_system_sound(SystemSoundEnum.exit)
                 logger.debug("Simulate exit")
+                exit()
+            if "打开浏览器" in prediction.transcript:
+                self.browser = Browser(config.external_tool.browser)
+                self.browser.open("https://www.bing.com")
+            if "关闭浏览器" in prediction.transcript:
+                self.browser.close()
+            if "搜索" in prediction.transcript:
+                self.browser.move_to_search_box()
+                text = prediction.transcript[2:]
+                self.browser.send_keys_and_enter(text)
+            if "错误" in prediction.transcript:
+                self.speaker.play_system_sound(SystemSoundEnum.error)
+            if "警告" in prediction.transcript:
+                self.speaker.play_system_sound(SystemSoundEnum.warn)
+            if "关闭麦克风" in prediction.transcript:
+                self.vad_emitter.stop()
 
     def on_llm(self):
         @self.emitter.on("llm")
         async def llm_query_handler(prediction: LLMPrediction):
             logger.info("LLM: " + prediction.response)
             tts_prompt = self.speech_manager.default_tts_prompt
-            query = TTSQuery(
+            query = GPT_SoVITS_TTS_Query(
                 text=prediction.response,
                 text_language="zh",
                 refer_wav_path=tts_prompt.audio_path,
                 prompt_text=tts_prompt.prompt_text,
                 prompt_language=tts_prompt.lang,
+                cut_punc="，。！",
             )
             for prediction in self.tts.stream_predict(query):
                 await self.emitter.emit("tts", prediction)
@@ -102,11 +123,11 @@ class ZerolanLiveRobot:
         async def tts_handler(prediction: TTSPrediction):
             self.speaker.playsound(prediction.wave_data, block=True)
 
-    def on_open(self):
-        # @self.emitter.on("cnm")
-        ...
-
 
 if __name__ == '__main__':
+    # ui_process = Process(target=zerolan.ui.app.start_ui_application, daemon=True)
+    # ui_process.start()
     bot = ZerolanLiveRobot()
     asyncio.run(bot.start())
+    # ui_process.kill()
+    # ui_process.join()
