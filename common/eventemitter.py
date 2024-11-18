1a6d029407cd4278a516b6bd5bc9246d19ce6d84
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-18 15:27:45 +0800

    refactor: use enum

--- common/eventemitter.py
+++ common/eventemitter.py
@@ -1,39 +1,55 @@
 import asyncio
 import inspect
-from typing import Dict, List, Callable, Coroutine, Literal
+from enum import Enum
+from typing import Dict, List, Callable, Coroutine, Union
 
 from loguru import logger
 
-_T = Literal[
-    "service.live_stream.disconnected",
-    "service.live_stream.danmaku",
-    "service.live_stream.gift",
-    "service.live_stream.super_chat",
-    "service.vad.speech_chunk",
-    "pipeline.asr",
-    "pipeline.llm",
-    "pipeline.tts",
-    "service.game.login",
-    "service.game.chat",
-    "error",
-    "crashed"
-]
+
+class EventEnum(str, Enum):
+    ...
+
+
+class Event:
+    class Pipeline(EventEnum):
+        ASR = "pipeline.asr"
+        LLM = "pipeline.llm"
+        TTS = "pipeline.tts"
+
+    class Service:
+        class LiveStream(EventEnum):
+            Disconnected = "service.live_stream.disconnected"
+            Danmaku = "service.live_stream.danmaku"
+            Gift = "service.live_stream.gift"
+            SuperChat = "service.live_stream.super_chat"
+
+        class VAD(EventEnum):
+            SpeechChunk = "service.vad.speech_chunk"
+
+        class Game(EventEnum):
+            Connected = "service.game.connected"
+            Disconnected = "service.game.disconnected"
+
+    class System(EventEnum):
+        Error = "system.error"
+        Crashed = "system.crashed"
 
 
 class EventEmitter:
     def __init__(self):
         self.listeners: Dict[str, List[Callable]] = {}
 
-    def add_listener(self, event, listener):
+    def add_listener(self, event: str, listener):
         if self.listeners.get(event) is None:
             self.listeners[event] = []
         self.listeners[event].append(listener)
         logger.debug(f"Current listeners {len(self.listeners[event])}")
 
-    def on(self, event: _T) -> Callable:
+    def on(self, event: EventEnum) -> Callable:
         # assert event in _T, f"{event} not in {_T}"
+
         def decorator(func: Coroutine):
-            self.add_listener(event, func)
+            self.add_listener(event.name, func)
             return func
 
         return decorator
@@ -42,19 +58,19 @@ class EventEmitter:
         try:
             task(*args, **kwargs)
         except Exception as e:
-            asyncio.gather(self.emit("error", e))
+            asyncio.gather(self.emit(Event.System.Error, e))
 
     async def _handle_async_task(self, task):
         try:
             await task
         except Exception as e:
-            await self.emit("error", e)
+            await self.emit(Event.System.Error, e)
 
-    async def emit(self, event: _T, *args, **kwargs) -> None:
-        listeners = self.listeners.get(event, None)
+    async def emit(self, event: EventEnum, *args, **kwargs) -> None:
+        listeners = self.listeners.get(event.name, None)
         if listeners is None:
-            if event == "error":
-                await emitter.emit("crashed", args)
+            if event == Event.System.Error:
+                await emitter.emit(Event.System.Crashed, args)
             return
         tasks = []
         for listener in listeners:
@@ -65,9 +81,10 @@ class EventEmitter:
                 self._handle_sync_task(listener(*args, **kwargs))
 
         await asyncio.gather(*tasks)
-        logger.debug(f"Event {event} emitted")
+        logger.debug(f"Event {event.name} emitted")
 
     def stop(self):
         self.listeners.clear()
+
 
 emitter = EventEmitter()
