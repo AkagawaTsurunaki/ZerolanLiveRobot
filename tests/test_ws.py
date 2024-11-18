e3308f52332fa577e260dca592674a06006ef27f
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-18 23:41:08 +0800

    test: Register instructions from Koneko test passed

--- tests/test_ws.py
+++ tests/test_ws.py
@@ -1,29 +1,48 @@
-import asyncio
 import threading
-import time
 
-from langchain_core.messages import ToolCall
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.tools import BaseTool
 
-from services.game.minecraft.app import KonekoMinecraftAIAgent
-from services.game.minecraft.data import KonekoProtocol
+from agent.tool_agent import Tool, Function, Parameters, Property, ToolAgent
+from common.config import get_config
+from services.game.minecraft.app import KonekoMinecraftAIAgent, WebSocketServer
 
-ws = KonekoMinecraftAIAgent(host="127.0.0.1", port=10098)
+ws = WebSocketServer()
+agent = KonekoMinecraftAIAgent(ws)
 
 
-async def test_ws():
-    ws_thr = threading.Thread(target=ws.start)
+def test_ws():
+    ws_thr = threading.Thread(target=agent.start)
     ws_thr.start()
-    # ws.start()
 
-    while True:
-        time.sleep(1)
-        protocolObj = KonekoProtocol(data=ToolCall(name="chat", args={"content": "Ciallo!"}, id="144"),
-                                     type="instruction")
-        await ws.send_message(protocolObj)
-        print("Send")
+    # l = [
+    #     Tool(type="function",
+    #          function=Function(name="chat", description="asasd",
+    #                            parameters=Parameters(
+    #                                properties={"content": Property(description="用于与大家聊天", type="string"),
+    #                                            # "time": Property(description="asasd", type="number"),
+    #                                            },
+    #                                required=["content"],
+    #                                type="ChatInstruction")))
+    # ]
+    # tool = agent._instructions[0]
+    # config = get_config()
+    # model = ToolAgent(config=config.pipeline.llm)
+    # tools = [tool]
+    #
+    # model.bind_tools(tools)
+    # messages = [model.system_prompt, HumanMessage("我要与大家聊天，跟大家说：你好！这里是一则通知")]
+    # ai_msg: AIMessage = model.invoke(messages)
+    # messages.append(ai_msg)
+    # for tool_call in ai_msg.tool_calls:
+    #     selected_tool: BaseTool = tool
+    #     tool_msg = selected_tool.invoke(tool_call)
+    #     messages.append(tool_msg)
+    # print(messages)
+    # tool.invoke(ToolCall(id="asdsad", args={"content": "Ciallo"}, name="chat"))
 
-    # t.join()
+
     ws_thr.join()
 
 
-asyncio.run(test_ws())
+test_ws()
