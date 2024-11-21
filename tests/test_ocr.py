09f6bbe83c698c92b3495254d60d212c88cdabee
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-21 21:48:21 +0800

    refactor: Remove test files

--- tests/test_ocr.py
+++
@@ -1,9 +0,0 @@
-from services.ocr.pipeline import OCRQuery
-from pipeline.ocr import OCRPipeline
-
-img_path = R"..\res\static\image\ocr-test-zh.png"
-
-pipeline = OCRPipeline()
-q = OCRQuery(img_path)
-p = pipeline.predict(q)
-print(p.unfold_as_str())
--- tests/test_vidcap.py
+++
@@ -1,9 +0,0 @@
-from services.vid_cap.pipeline import VidCapQuery
-from pipeline.vid_cap import VidCapPipeline
-
-pipeline = VidCapPipeline()
-q = VidCapQuery(
-    vid_path=R"..\res\static\vid\vidcap-test.mp4")
-
-p = pipeline.predict(query=q)
-print(p.caption)
