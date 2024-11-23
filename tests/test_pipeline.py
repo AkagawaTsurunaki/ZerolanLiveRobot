f072b4d5d0baccae41446586e75de9b819cecf18
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-23 14:42:38 +0800

    test: Test for tts pipeline

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -12,11 +12,6 @@ llm = LLMPipeline(config.pipeline.llm)
 tts = TTSPipeline(config.pipeline.tts)
 
 
-def test_tts_prompt_manager():
-    manager = TTSPromptManager(config=config.character.speech)
-    assert len(manager.tts_prompts) > 0, f"{manager.tts_prompts} should not be empty."
-
-
 def test_llm():
     query = LLMQuery(text="Hello world!", history=[])
     prediction = llm.predict(query)
@@ -24,17 +19,18 @@ def test_llm():
 
 
 def test_tts():
-    # Remember to change these variables
-    refer_wav_path = ""
-    prompt_text = ""
-    prompt_language = ""
+    manager = TTSPromptManager(config=config.character.speech)
+    assert len(manager.tts_prompts) > 0, f"{manager.tts_prompts} should not be empty."
+    prompt = manager.tts_prompts[0]
     query = TTSQuery(text="你好！能听见我说话吗？",
                      text_language="zh",
-                     refer_wav_path=refer_wav_path,
-                     prompt_text=prompt_text,
-                     prompt_language=prompt_language)
+                     refer_wav_path=prompt.audio_path,
+                     prompt_text=prompt.prompt_text,
+                     prompt_language=prompt.lang)
     prediction = tts.predict(query)
     assert prediction, f"No prediction from TTS pipeline."
+    assert prediction.wave_data is not None and len(
+        prediction.wave_data) > 0, f"No audio data returned from TTS pipeline."
 
 
 def test_asr():
