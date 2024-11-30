685739af86cf2932e593c19bca8253bea2ebdc19
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-30 23:59:44 +0800

    fix: Version changes and fix the block issue

--- services/vad/emitter.py
+++ services/vad/emitter.py
@@ -5,9 +5,10 @@ from loguru import logger
 
 from common.decorator import withsound
 from common.enumerator import SystemSoundEnum, EventEnum
-from event.eventemitter import emitter
 from common.limit_list import LimitList
 from common.utils.audio_util import from_ndarray_to_bytes
+from event.event_data import SpeechEvent
+from event.eventemitter import emitter, EventEmitter, TypedEventEmitter
 from services.device.microphone import Microphone
 from services.vad.strategy import EasyEnergyVad
 
@@ -25,6 +26,11 @@ class VoiceEventEmitter:
         self.mp.open()
         asyncio.run(self.handler())
 
+    @withsound(SystemSoundEnum.enable_func)
+    async def astart(self):
+        self.mp.open()
+        await self.handler()
+
     @withsound(SystemSoundEnum.disable_func)
     def stop(self):
         self._stop_flag = True
@@ -34,6 +40,8 @@ class VoiceEventEmitter:
         for chunk in self.mp.stream():
             if self._stop_flag:
                 break
+
+            await asyncio.sleep(0)
             speech_chunk = np.frombuffer(chunk, dtype=np.float32)
             speech = self.vad.check_stream(speech_chunk)
             if speech is None:
@@ -41,10 +49,15 @@ class VoiceEventEmitter:
                     combined_speech_bytes = from_ndarray_to_bytes(np.concatenate(self.speech_chunks),
                                                                   sample_rate=self.mp.sample_rate)
                     logger.info("Voice event emitted")
-                    await emitter.emit(EventEnum.SERVICE_VAD_SPEECH_CHUNK,
-                                       speech=combined_speech_bytes,
-                                       channels=self.mp.channels,
-                                       sample_rate=self.mp.sample_rate)
+                    if isinstance(emitter, EventEmitter):
+                        await emitter.emit(EventEnum.SERVICE_VAD_SPEECH_CHUNK,
+                                           speech=combined_speech_bytes,
+                                           channels=self.mp.channels,
+                                           sample_rate=self.mp.sample_rate)
+                    elif isinstance(emitter, TypedEventEmitter):
+                        emitter.emit(SpeechEvent(speech=combined_speech_bytes,
+                                                 channels=self.mp.channels,
+                                                 sample_rate=self.mp.sample_rate))
 
                     self.speech_chunks.clear()
             else:
