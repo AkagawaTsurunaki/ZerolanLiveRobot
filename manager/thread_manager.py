093c1f7a4ad37205bde8f48e04791fd55606d25c
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-30 14:28:56 +0800

    feat: add_thread and start_all

--- manager/thread_manager.py
+++ manager/thread_manager.py
@@ -5,8 +5,18 @@ class ThreadManager:
     def __init__(self):
         self._threads: dict[str, threading.Thread] = {}
 
-    def start_thread(self, thread: threading.Thread):
+    def add_thread(self, thread: threading.Thread):
+        if self._threads.get(thread.name, None) is not None:
+            raise RuntimeError("Duplicate thread name in ThreadManager.")
         self._threads[thread.name] = thread
+
+    def start_all(self):
+        for name, t in self._threads.items():
+            t.start()
+            logger.info(f"Thread {name} started.")
+
+    def start_thread(self, thread: threading.Thread):
+        self.add_thread(thread)
         thread.start()
         logger.info(f"Thread {thread.name} started.")
 
