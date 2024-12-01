69d292f14cae595f9b950014a38c747faa019df1
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-01 14:25:35 +0800

    refactor: Use old version of WebSocketServer

--- services/live2d/app.py
+++ services/live2d/app.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 
 from common.config import Live2DConfig
 from common.enumerator import EventEnum
-from event.websocket import WebSocketServer
+from event.websocket import OldWebSocketServer
 
 
 class ZerolanLive2DProtocol(BaseModel):
@@ -21,7 +21,7 @@ class Live2dApplication:
     def __init__(self, config: Live2DConfig):
         self._model_dir = abspath(config.model_dir)
         assert os.path.exists(self._model_dir), f"{self._model_dir} does not exist."
-        self._ws = WebSocketServer(config.host, config.port)
+        self._ws = OldWebSocketServer(config.host, config.port)
 
     def start(self):
         self.init()
