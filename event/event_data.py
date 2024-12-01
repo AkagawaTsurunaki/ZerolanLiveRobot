a35268a6af355c1cae3186916d32c23e05c077fa
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-01 10:24:26 +0800

    feat: Add new event data

--- event/event_data.py
+++ event/event_data.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from typing import List
 
 from zerolan.data.pipeline.asr import ASRPrediction
 
+from agent.tool_agent import Tool
 from common.enumerator import EventEnum
+from services.game.minecraft.data import KonekoProtocol
 
 
 class BaseEvent:
@@ -21,3 +24,20 @@ class SpeechEvent(BaseEvent):
     channels: int
     sample_rate: int
     type: EventEnum = EventEnum.SERVICE_VAD_SPEECH_CHUNK
+
+
+@dataclass
+class KonekoClientPushInstructionsEvent(BaseEvent):
+    tools: List[Tool]
+    type: EventEnum = EventEnum.KONEKO_CLIENT_PUSH_INSTRUCTIONS
+
+
+@dataclass
+class KonekoClientHelloEvent(BaseEvent):
+    type = EventEnum.KONEKO_CLIENT_HELLO
+
+
+@dataclass
+class KonekoServerCallInstruction(BaseEvent):
+    protocol_obj: KonekoProtocol
+    type = EventEnum.KONEKO_SERVER_CALL_INSTRUCTION
