133a564aaf8a0c0f45b47fb5f82c61ef9b793978
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-01 10:01:34 +0800

    feat: Async and sync methods

--- common/abs_runnable.py
+++ common/abs_runnable.py
@@ -1,13 +1,21 @@
+import uuid
 from abc import abstractmethod
+from typing import Dict
 
 
 class AbstractRunnable:
 
     def __init__(self):
         self._activate: bool = False
+        self._id: str = str(uuid.uuid4())
 
     @abstractmethod
-    async def start(self):
+    async def astart(self):
+        self.start()
+
+    @abstractmethod
+    def start(self):
+        _all[self._id] = self
         self._activate = True
 
     def activate_check(self):
@@ -15,5 +23,25 @@ class AbstractRunnable:
             raise RuntimeError("This runnable object is not activated. Call `start()` first.")
 
     @abstractmethod
-    async def stop(self):
+    async def astop(self):
+        self.stop()
+
+    @abstractmethod
+    def stop(self):
+        assert _all.pop(self._id, None) is not None, f"Some runnable object is ignored? This should not happen!"
         self._activate = False
+
+
+# 所有的可运行组件都应该在调用 `start` 方法的时候被注册在这里
+# All runnable components should be registered here when the `start` method is called
+_all: Dict[str, AbstractRunnable] = {}
+
+
+async def stop_all():
+    """
+    强制停止所有可运行组件的运行
+    Force stop the operation of all runnable components
+    """
+    global _all
+    for id, run in _all.items():
+        await run.astop()
