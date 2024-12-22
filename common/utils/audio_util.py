a67e1bc8469f720ceee2b9930a1be08b5d33f7aa
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-22 17:17:23 +0800

    feat: Support wave info

--- common/utils/audio_util.py
+++ common/utils/audio_util.py
@@ -1,5 +1,7 @@
 import io
 
+from pydub import AudioSegment
+
 from common.utils.file_util import create_temp_file
 
 
@@ -11,19 +13,28 @@ def from_ndarray_to_bytes(speech_chunk, sample_rate):
     return wave_file.getvalue()
 
 
-def check_wav_info(file_path) -> (int, int, float):
-    import wave
-    # 打开WAV文件
-    wav_file = wave.open(file_path, mode='rb')
-    # 获取采样率
-    sample_rate = wav_file.getframerate()
-    # 获取通道数
-    num_channels = wav_file.getnchannels()
-    # 获取时长
-    duration = wav_file.getnframes() / sample_rate
-    # 关闭文件
-    wav_file.close()
+def check_audio_info(file_path) -> (int, int, float):
+    audio = AudioSegment.from_ogg(file_path)
+    sample_rate = audio.frame_rate
+    num_channels = audio.channels
+    duration_ms = len(audio)
+    duration = duration_ms / 1000.0
     return sample_rate, num_channels, duration
+
+
+# def check_wav_info(file_path) -> (int, int, float):
+#     import wave
+#     # 打开WAV文件
+#     wav_file = wave.open(file_path, mode='rb')
+#     # 获取采样率
+#     sample_rate = wav_file.getframerate()
+#     # 获取通道数
+#     num_channels = wav_file.getnchannels()
+#     # 获取时长
+#     duration = wav_file.getnframes() / sample_rate
+#     # 关闭文件
+#     wav_file.close()
+#     return sample_rate, num_channels, duration
 
 
 def check_audio_format(audio) -> str:
