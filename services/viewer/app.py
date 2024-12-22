925a0433d3b9797f3a3fd97f32ab0f09e8785b89
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-22 17:18:35 +0800

    refactor: Extract as method play_speech

--- services/viewer/app.py
+++ services/viewer/app.py
@@ -5,7 +5,7 @@ from loguru import logger
 from pydantic import BaseModel
 from zerolan.data.protocol.protocol import ZerolanProtocol
 
-from common.utils.audio_util import check_audio_format
+from common.utils.audio_util import check_audio_format, check_audio_info
 from event.websocket import ZerolanProtocolWebsocket
 
 
@@ -14,6 +14,9 @@ class PlaySpeechDTO(BaseModel):
     audio_uri: str
     transcript: str
     audio_type: str
+    sample_rate: int
+    channels: int
+    duration: float
 
 
 def path_to_uri(path):
@@ -34,22 +37,23 @@ class ZerolanViewerServer(ZerolanProtocolWebsocket):
             zp = ZerolanProtocol(protocol="ZerolanViewerProtocol", version="1.0", message="Server hello!", code=0,
                                  action="server_hello", data=None)
             await self._ws.send_json(zp.model_dump())
-            logger.debug("Sent")
-            # elif data.action == "server_hello":
-            audio_path = R"D:\AkagawaTsurunaki\WorkSpace\PythonProjects\ZerolanLiveRobot\resources\static\prompts\tts\[zh][Default]喜欢游戏的人和擅长游戏的人有很多不一样的地方，老师属于哪一种呢？.wav"
-            audio_type = check_audio_format(audio_path)
-            audio_uri = path_to_uri(audio_path)
-            ana = PlaySpeechDTO(bot_id="UnityChan", audio_uri=audio_uri,
-                                transcript="喜欢游戏的人和擅长游戏的人有很多不一样的地方，老师属于哪一种呢？",
-                                audio_type=audio_type)
-            zp = ZerolanProtocol(protocol="ZerolanViewerProtocol", version="1.0",
-                                 message="Add the speech into a queue and waiting for playing", code=0,
-                                 action="play_speech", data=ana)
-            await self._ws.send_json(zp.model_dump())
-            logger.debug("Sent")
-        # if data.action == "add_audio":
-        #     ...
-        # else
+
+    async def play_speech(self, bot_id: str, audio_path: str, transcript: str):
+        audio_type = check_audio_format(audio_path)
+        sample_rate, num_channels, duration = check_audio_info(audio_path)
+        audio_uri = path_to_uri(audio_path)
+        ana = PlaySpeechDTO(bot_id=bot_id, audio_uri=audio_uri,
+                            transcript=transcript,
+                            audio_type=audio_type,
+                            sample_rate=sample_rate,
+                            channels=num_channels,
+                            duration=duration
+                            )
+        zp = ZerolanProtocol(protocol="ZerolanViewerProtocol", version="1.0",
+                             message="Add the speech into a queue and waiting for playing", code=0,
+                             action="play_speech", data=ana)
+        await self._ws.send_json(zp.model_dump())
+        logger.debug("Sent")
 
 
 async def run():
