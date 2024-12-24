ab558e1c193547bb70942b500e2995941877022d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-24 16:27:05 +0800

    fix: Load json like object

--- common/utils/json_util.py
+++ common/utils/json_util.py
@@ -1,48 +1,65 @@
+"""
+Author: Github@AkagawaTsurunaki
+Notes:
+    GLM-4 输出的 JSON 结果可能会带有 Markdown 代码块标记，以及出现的多余的右大括号。
+    提示词调整后依旧无效，为了防止还有其他模型具有相同错误，特使用此模块从一个错误的 JSON 字符串中尝试恢复。
+"""
+
 import json
-import re
 from json import JSONDecodeError
 
 
-def _extract_json_from_markdown(markdown_text: str) -> str:
-    cleaned_text = re.sub(r'```.*?\n(.*?)\n```', r'\1', markdown_text, flags=re.DOTALL)
-    return cleaned_text
+def _extract_json_from_text(text: str):
+    """
+    从如下的文档中获取JSON字符串：
+    ...
+    ```json
+    {
+        ...
+    }
+    ```
+    ...
+    文档中必须只有一个 JSON 对象。
+    Args:
+        text:
+
+    Returns:
+    """
+    start, end = 0, len(text)
+    for i in range(len(text)):
+        if text[i] == "{" or text[i] == "[":
+            start = i
+            break
+    for i in range(len(text)):
+        j = len(text) - i - 1
+        if text[j] == "}" or text[j] == "]":
+            end = j
+            break
+    return text[start:end + 1]
 
 
-def _remove_extra_braces(json_string):
-    for i in range(len(json_string) - 1, 0, -1):
-        if json_string[i] == '}':
-            json_string = json_string[:i] + json_string[i + 1:]
-            return json_string
+def _remove_end_extra_braces(text: str):
+    """
+    {...}
+    Args:
+        text:
 
+    Returns:
 
-def _normal_load(content: str):
-    return json.loads(content)
-
-
-def _md_load(content: str):
-    content = _extract_json_from_markdown(content)
-    return _normal_load(content)
-
-
-def _ebr_load(content: str):
-    content = _remove_extra_braces(content)
-    return _normal_load(content)
-
-
-def _md_ebr_load(content: str):
-    content = _extract_json_from_markdown(content)
-    content = _remove_extra_braces(content)
-    return _normal_load(content)
-
-
-_strategies = [_normal_load, _md_load, _ebr_load, _md_ebr_load]
-
-
-def smart_load_json_like(content: str):
+    """
     errs = []
-    for strategy in _strategies:
+    n = text.count("}")
+    for i in range(n):
+        j = len(text) - i
+        new_text = text[:j]
         try:
-            return strategy(content)
+            return json.loads(new_text)
         except JSONDecodeError as e:
             errs.append(e)
     raise errs[-1]
+
+
+def smart_load_json_like(content: str):
+    json_val = _extract_json_from_text(content)
+    json_val = _remove_end_extra_braces(json_val)
+    return json_val
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,3 +1,5 @@
+import json
+
 from common.utils.json_util import smart_load_json_like
 
 json_vals = [
@@ -19,7 +21,7 @@ json_vals = [
     {
         "text": "Hello",
         "number": 1.1
-    }    
+    }
     ```
     """,
     """
@@ -27,7 +29,7 @@ json_vals = [
     {
         "text": "Hello",
         "number": 1.1
-    }} 
+    }}
     ```
     """,
     """
@@ -36,7 +38,21 @@ json_vals = [
     {
         "text": "Hello",
         "number": 1.1
-    }} 
+    }}
+    ```
+    如果你还有其他需要，请随时使用我。
+    """,
+    """
+    以下是生成的 json 数据：
+    ```json
+    {"name": "游戏对象创建器", "args": {"instance_id": 1, "gameobject_name": "绿色立方体", "object_type": "cube", "color": "#008000", "transform": {"scale": 1, "position": {"x": 0, "y": 0, "z": 0}}}}}}
+    ```
+    如果你还有其他需要，请随时使用我。
+    """,
+    """
+    以下是生成的 json 数据：
+    ```json
+    {"name": "游戏对象创建器", "args": {"instance_id": 1, "gameobject_name": "绿色立方体", "object_type": "cube", "color": "#008000", "transform": {"scale": 1, "position": {"x": 0, "y": 0, "z": 0}}}}}}}}}
     ```
     如果你还有其他需要，请随时使用我。
     """
@@ -47,4 +63,4 @@ def test_json():
     for i, json_val in enumerate(json_vals):
         json_val = smart_load_json_like(json_val)
         print(f"Test case {i} passed:")
-        print(json_val)
+        print(json.dumps(json_val, indent=4, ensure_ascii=False))
