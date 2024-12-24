fad96f1888e7f1cd1ff009ed724000ba2f2bfc50
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-12-24 11:07:04 +0800

    test: Test for model_scale

---
+++ tests/test_viewer.py
@@ -0,0 +1,26 @@
+import asyncio
+from asyncio import TaskGroup
+
+from agent.model_modifier import model_scale
+from services.viewer.app import ZerolanViewerServer
+
+viewer = ZerolanViewerServer(host="0.0.0.0", port=11013, protocol="ZerolanViewerProtocol", version="1.0")
+
+
+async def run():
+    async with TaskGroup() as tg:
+        tg.create_task(viewer.start())
+
+        while not viewer.is_connected:
+            await asyncio.sleep(1)
+
+        await asyncio.sleep(1)
+        print("准备获取")
+        info = viewer.get_gameobjects_info()
+        s = model_scale(info, "请把UnityChan的模型放大两倍")
+        print(s)
+        await viewer.modify_game_object_scale(s)
+
+
+def test_model_scale():
+    asyncio.run(run())
