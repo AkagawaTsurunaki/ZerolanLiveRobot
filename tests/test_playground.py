77f2123fee9509497db0de48de2733fbad82615d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-01-19 14:31:48 +0800

    test: test_create_gameobject and test_modify_game_object_scale passed

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -4,7 +4,7 @@ from asyncio import TaskGroup
 import pytest
 
 from common.config import get_config
-from common.data import LoadLive2DModelDTO
+from common.data import LoadLive2DModelDTO, CreateGameObjectDTO, GameObjectType, Transform, Position, ScaleOperationDTO
 from manager.model_manager import ModelManager
 from services.playground.bridge import PlaygroundBridge
 
@@ -60,3 +60,28 @@ async def test_load_live2d_model():
         await syncwait()
         await _bridge.load_live2d_model(
             LoadLive2DModelDTO(bot_id=bot_id, model_dir=model_dir, bot_display_name="Koneko"))
+
+
+@pytest.mark.asyncio
+async def test_create_gameobject():
+    async with TaskGroup() as tg:
+        tg.create_task(test_conn())
+        await syncwait()
+        await _bridge.create_gameobject(
+            CreateGameObjectDTO(instance_id=114, game_object_name="MySphere", object_type=GameObjectType.SPHERE,
+                                color="#114514", transform=Transform(scale=5.0, position=Position(x=1, y=1, z=1))))
+
+
+@pytest.mark.asyncio
+async def test_modify_game_object_scale():
+    async with TaskGroup() as tg:
+        tg.create_task(test_conn())
+        await syncwait()
+        await _bridge.create_gameobject(
+            CreateGameObjectDTO(instance_id=114, game_object_name="MySphere", object_type=GameObjectType.SPHERE,
+                                color="#114514", transform=Transform(scale=5.0, position=Position(x=1, y=1, z=1))))
+        await asyncio.sleep(1)
+        for e in _bridge.get_gameobjects_info():
+            print(e.game_object_name)
+            if e.game_object_name == "MySphere":
+                await _bridge.modify_game_object_scale(ScaleOperationDTO(instance_id=e.instance_id, target_scale=0.5))
