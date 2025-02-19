99087221cfb85784d7e6d44b6895703e4d78f3a9
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-02-19 17:14:34 +0800

    test: test_ws passed

--- tests/test_ws.py
+++ tests/test_ws.py
@@ -1,22 +1,14 @@
-import asyncio
+from asyncio import TaskGroup
 
+import pytest
 from loguru import logger
 
 from event.websocket import WebSocketServer
 
 
-async def main():
-    ws = WebSocketServer('127.0.0.1', 8111)
-    ws_task = asyncio.create_task(ws.start())
-    await asyncio.sleep(4)
-    await ws.send_json({"test": "1"})
-    logger.info("Sent message")
-    await asyncio.sleep(0.5)
-    await ws.stop()
-
-    await ws_task
+@pytest.mark.asyncio
+async def test_ws():
+    ws = WebSocketServer('127.0.0.1', 11013, match_sub_protocol="ZerolanProtocol")
+    async with TaskGroup() as tg:
+        tg.create_task(ws.start())
     logger.info("Test passed")
-
-
-def test_ws():
-    asyncio.run(main())
