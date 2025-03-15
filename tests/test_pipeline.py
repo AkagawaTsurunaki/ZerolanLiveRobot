dca61dbc8e118e7cd72046ff7bf4be4e3878e597
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-15 16:46:08 +0800

    test: TTS pipeline test cases passed

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,18 +1,21 @@
+import time
+
 from zerolan.data.pipeline.asr import ASRQuery
 from zerolan.data.pipeline.img_cap import ImgCapQuery
 from zerolan.data.pipeline.llm import LLMQuery, Conversation, RoleEnum
 from zerolan.data.pipeline.ocr import OCRQuery
 from zerolan.data.pipeline.tts import TTSQuery
 from zerolan.data.pipeline.vla import ShowUiQuery, WebAction
-
-from common.config import get_config
-from common.enumerator import Language
 from zerolan.ump.pipeline.asr import ASRPipeline
 from zerolan.ump.pipeline.img_cap import ImgCapPipeline
 from zerolan.ump.pipeline.llm import LLMPipeline
 from zerolan.ump.pipeline.ocr import OCRPipeline
 from zerolan.ump.pipeline.tts import TTSPipeline
 from zerolan.ump.pipeline.vla import ShowUIPipeline
+
+from common.config import get_config
+from common.decorator import log_run_time
+from common.enumerator import Language
 from services.device.microphone import Microphone
 
 _config = get_config()
@@ -44,13 +47,88 @@ def test_llm_history():
 def test_tts():
     query = TTSQuery(text="你好！能听见我说话吗？",
                      text_language=Language.ZH,
-                     refer_wav_path="resources/tts-test.wav",
+                     refer_wav_path="",
                      prompt_text="我是赤川鹤鸣",
                      prompt_language=Language.ZH)
-    prediction = tts.predict(query)
+
+    @log_run_time()
+    def tts_predict():
+        return tts.predict(query)
+
+    prediction = tts_predict()
     assert prediction, f"No prediction from TTS pipeline."
     assert prediction.wave_data is not None and len(
         prediction.wave_data) > 0, f"No audio data returned from TTS pipeline."
+
+
+CHUNK_SIZE = 1024  # 每次处理的音频数据大小
+
+import pyaudio
+
+FORMAT = pyaudio.paInt16  # 音频格式（16位整数）
+CHANNELS = 1  # 单声道
+RATE = 32000  # 采样率（16kHz）
+# 初始化PyAudio
+p = pyaudio.PyAudio()
+
+# 打开音频流
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                output=True)
+
+
+def close_pyaudio():
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+
+def test_tts2():
+    times = []
+
+    t_start_post = time.time()
+    print(f"Start post: {t_start_post}")
+
+    @log_run_time()
+    def llm_predict():
+        query = LLMQuery(text="你现在能和我玩游戏吗？",
+                         history=[Conversation(role=RoleEnum.user, content="你现在是一只猫娘，请在句尾始终带上喵"),
+                                  Conversation(role=RoleEnum.assistant, content="好的，主人喵")])
+        prediction = llm.predict(query)
+        return prediction
+
+    prediction = llm_predict()
+    query = TTSQuery(text=prediction.response,
+                     text_language=Language.ZH,
+                     refer_wav_path="",
+                     prompt_text="",
+                     prompt_language=Language.ZH)
+
+    first_rcv = False
+    for prediction in tts.stream_predict(query):
+        if not first_rcv:
+            first_rcv = True
+            t_first_chunk = time.time()
+            print(f"Post data got: {time.time()}")
+            print(f"Elapsed time: {t_first_chunk - t_start_post}")
+            times.append(t_first_chunk - t_start_post)
+        stream.write(prediction.wave_data)  # 将音频数据写入音频流并播放
+
+    # 大幅度提升速度，从原来的1.5s延时提升到0.5秒以内，提升超过3倍。
+    print(f"Len: {times}")
+    print(f"Avg: {sum(times) / len(times)}")
+    close_pyaudio()
+
+
+def test_tts3():
+    query = TTSQuery(text="你好！能听见我说话吗？",
+                     text_language=Language.ZH,
+                     refer_wav_path="",
+                     prompt_text="",
+                     prompt_language=Language.ZH)
+    for prediction in tts.stream_predict(query):
+        print(prediction.seq)
 
 
 def test_asr():
