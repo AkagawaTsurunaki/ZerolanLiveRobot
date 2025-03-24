8911ff5e8b7feb4162fb54aa4e3851c891f4b088
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-24 16:41:42 +0800

    refactor: Use Field to set ObsStudioClientConfig

--- services/obs/config.py
+++ services/obs/config.py
@@ -1,13 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ObsStudioClientConfig(BaseModel):
-    enable: bool = True
-    # URI for connection with target OBS studio WebSocket server. For example: ws://127.0.0.1:4455
-    uri: str = "ws://127.0.0.1:4455"
-    # Password for connection with target OBS studio Websocket server. Select Tool > WebSocket Server Setting > Show Connect Info, and you can find Server Password.
-    password: str
-    # The name of specific Text (GDI+) input component in the `Sources` panel. Display assistant output text.
-    assistant_text_comp_name: str = "AssistantText"
-    # The name of specific Text (GDI+) input component in the `Sources` panel. Display user input text.
-    user_text_comp_name: str = "UserText"
+    enable: bool = Field(default=True,
+                         description="Enable system connect to OBS studio to control the Text component for displaying subtitles?")
+    uri: str = Field(default="ws://127.0.0.1:4455",
+                     description="URI for connection with target OBS studio WebSocket server. \n"
+                                 "For example: ws://127.0.0.1:4455")
+    password: str = Field(default="<PASSWORD>",
+                          description="Password for connection with target OBS studio Websocket server. \n"
+                                      "Select Tool > WebSocket Server Setting > Show Connect Info, and you can find Server Password.")
+    assistant_text_comp_name: str = Field(default="AssistantText",
+                                          description="The name of specific Text (GDI+) input component in the `Sources` panel. \n"
+                                                      "Display assistant output text.")
+    #
+    user_text_comp_name: str = Field(default="UserText",
+                                     description="The name of specific Text (GDI+) input component in the `Sources` panel. \n"
+                                                 "Display user input text.")
