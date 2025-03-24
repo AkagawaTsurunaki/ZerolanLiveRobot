2cfe32831d946e4fcb16f196b1ecb3e54dffb420
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-24 19:31:34 +0800

    feat: ConfigFileGenerator

--- common/gen.py
+++ common/gen.py
@@ -1,49 +1,43 @@
 from pydantic import BaseModel
-
-from common.config import ZerolanLiveRobotConfig
-
-
-def gen_config():
-    config = ZerolanLiveRobotConfig()
-    return config
+from pydantic.fields import FieldInfo
 
 
-yaml_str = ""
+class ConfigFileGenerator:
 
+    def __init__(self, indent: int = 2):
+        self._yaml_str = ""
+        self._indent = indent
 
-def model_to_yaml_with_comments(model: BaseModel, indent: int = 0) -> str:
-    global yaml_str
-    """
-    将 Pydantic 模型转换为带描述注释的 YAML 字符串。
-    :param model: Pydantic 模型实例
-    :param indent: 当前缩进级别（默认为 0）
-    :return: 带描述注释的 YAML 字符串
-    """
-    # 获取模型的字段信息
-    fields = model.model_fields
+    def _get_indent(self, depth: int):
+        return " " * self._indent * depth
 
-    # yaml_str = yaml.dump(model.model_dump(), allow_unicode=True)
+    def _add_comments(self, field_info: FieldInfo, depth: int):
+        if field_info.description:
+            for description_line in field_info.description.split("\n"):
+                self._yaml_str += self._get_indent(depth) + f"# {description_line}\n"
 
-    for field_name, field_info in fields.items():
-        description = field_info.description or ""
-        field_val = model.__getattribute__(field_name)
-        if isinstance(field_val, BaseModel):
-            # print(type(field_val))
-            if field_info.description:
-                for description_line in field_info.description.split("\n"):
-                    yaml_str += " " * indent + f"# {description_line}\n"
-            yaml_str += " " * indent + f"{field_name}:\n"
-            model_to_yaml_with_comments(field_val, indent + indent)
-        else:
-            if field_info.description:
-                for description_line in field_info.description.split("\n"):
-                    yaml_str += " " * indent + f"# {description_line}\n"
-            # kv = yaml.dump({field_name: field_val}, sys.stdout, allow_unicode=True)
-            # yaml_str += " " * indent + f"{kv}\n"
-            if isinstance(field_val, str):
-                yaml_str += " " * indent + f"{field_name}: '{field_val}'\n"
+    def _gen(self, model: BaseModel, depth: int = 0) -> str:
+        fields = model.model_fields
+
+        for field_name, field_info in fields.items():
+            field_val = model.__getattribute__(field_name)
+            if isinstance(field_val, BaseModel):
+                self._add_comments(field_info, depth)
+                self._yaml_str += self._get_indent(depth) + f"{field_name}:\n"
+                self._gen(field_val, depth + 1)
             else:
-                yaml_str += " " * indent + f"{field_name}: {field_val}\n"
-            # yaml_str += yaml.dump(field_val, allow_unicode=True) + "\n"
-        # print(field_name, description)
-    return yaml_str
+                self._add_comments(field_info, depth)
+                if isinstance(field_val, str):
+                    self._yaml_str += self._get_indent(depth) + f"{field_name}: '{field_val}'\n"
+                else:
+                    self._yaml_str += self._get_indent(depth) + f"{field_name}: {field_val}\n"
+        return self._yaml_str
+
+    def generate_yaml(self, model: BaseModel):
+        """
+        Generate yaml from BaseModel instance.
+        :param model: An instance of BaseModel.
+        :return: Yaml string.
+        """
+        assert model, f"None can not be generated."
+        return self._gen(model, depth=0)
