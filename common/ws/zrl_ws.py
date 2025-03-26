b7edd71e0c891741be0e5e0debd02b9fca1f055d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-26 15:49:51 +0800

    fix: Unpack data object can not be assigned to the field with type of Any

--- common/ws/zrl_ws.py
+++ common/ws/zrl_ws.py
@@ -5,9 +5,10 @@ Author: AkagawaTsurunaki
 from typing import Any
 
 from google.protobuf.any_pb2 import Any as ProtoBufAny  # type: ignore
+from loguru import logger
 from websockets import ProtocolError
 
-from common.ws.proto import protocol_pb2
+from common.ws.proto.protocol_pb2 import ZerolanProtocol  # type: ignore
 from common.ws.protoc_ws import ProtoBufWebSocketServer
 
 
@@ -27,23 +28,28 @@ class ZerolanProtocolWebSocket(ProtoBufWebSocketServer):
     def send(self, action: str, data: any, message: str = "", code: int = 0):  # noqa
         any_data = ProtoBufAny()
         any_data.Pack(data)
-        protocol_obj = protocol_pb2.ZerolanProtocol(protocol=self._protocol,  # type: ignore
-                                                    version=self._version,
-                                                    message=message,
-                                                    code=code,
-                                                    action=action,
-                                                    data=any_data)
+        protocol_obj = ZerolanProtocol(protocol=self._protocol,
+                                       version=self._version,
+                                       message=message,
+                                       code=code,
+                                       action=action,
+                                       data=any_data)
         super().send(protocol_obj)
 
-    def on_message(self, protoc_type: Any, action: str):  # noqa
+    def on_message(self, data_type: Any, action: str):  # noqa
         def decorator(func):
-            @super(ZerolanProtocolWebSocket, self).on_message(protoc_type)
-            def wrapper(connection, message):
-                assert isinstance(message, protoc_type)
+            @super(ZerolanProtocolWebSocket, self).on_message(ZerolanProtocol)
+            def on_zp_message(connection, message):
+                logger.debug("Test on_message")
+                assert isinstance(message, ZerolanProtocol)
                 if message.protocol == self._protocol and message.version == self._version:
                     if message.action == action:
-                        func(connection, message)
+                        data = data_type()
+                        message.data.Unpack(data)
+                        func(connection, data)
                 else:
                     raise ProtocolError("Invalid ZerolanProtocol. Check the protocol name and the version.")
+
+            logger.debug(f"Function {func.__name__} is registered as OnZerolanProtocolMessageListener")
 
         return decorator
