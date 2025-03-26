efb5f7e65937529ffdd5a3d18cb5c299b30228c2
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-26 23:38:02 +0800

    Revert "refactor: Operate model functions are removed"
    This reverts commit 14bfa81c42b069ea6292ebc664a35228d6ad8f75.

--- agent/api.py
+++ agent/api.py
@@ -16,8 +16,10 @@ from zerolan.data.pipeline.ocr import RegionResult
 
 from agent.adaptor import LangChainAdaptedLLM
 from common.config import get_config
+from services.playground.data import GameObjectInfo, ScaleOperationDTO
 from common.decorator import log_run_time
 from common.enumerator import Language
+from common.utils.json_util import smart_load_json_like
 from ump.pipeline.ocr import stringify
 
 _config = get_config()
@@ -169,3 +171,26 @@ def summary_history(history: List[Conversation]) -> AIMessage:
     response = _model.invoke(result)
 
     return response
+
+
+@log_run_time()
+def model_scale(info: List[GameObjectInfo], question: str) -> ScaleOperationDTO | None:
+    config = get_config()
+    model = LangChainAdaptedLLM(config=config.pipeline.llm)
+    format = {
+        "instance_id": int,
+        "target_scale": float
+    }
+
+    system_template = '你现在需要根据用户的指令，修改游戏对象的一些参数。返回的JSON格式为{format}'
+    user_template = '{info}【用户输入】{question}\n【你的任务】根据上面游戏对象的信息，识别用户需要操作的模型指令，将结果以JSON的形式返回。'
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", user_template)]
+    )
+
+    result = prompt_template.invoke({"format": format, "info": info, "question": question})
+    result.to_messages()
+    response = model.invoke(result)
+    json = smart_load_json_like(response.content)
+    return ScaleOperationDTO.model_validate(json)
---
+++ agent/tool/go_creator.py
@@ -0,0 +1,32 @@
+import asyncio
+from typing import Any, Type
+
+from injector import inject
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, Field
+
+from services.playground.data import CreateGameObjectDTO
+from services.playground.bridge import PlaygroundBridge
+
+
+class GameObjectCreatorInput(BaseModel):
+    dto: CreateGameObjectDTO = Field(description="DTO of the game object will be created")
+
+
+class GameObjectCreator(BaseTool):
+    name: str = "游戏对象创建器"
+    description: str = "当用户要求你创建一个游戏对象（例如立方体、球体）的时候，使用此工具。"
+    args_schema: Type[BaseModel] = GameObjectCreatorInput
+    return_direct: bool = True
+
+    @inject
+    def __init__(self, bridge: PlaygroundBridge):
+        super().__init__()
+        self._bridge = bridge
+
+    def _run(self, dto: CreateGameObjectDTO) -> Any:
+        task = [asyncio.create_task(self._arun(dto))]
+        asyncio.gather(*task)
+
+    async def _arun(self, dto: CreateGameObjectDTO) -> None:
+        await self._bridge.create_gameobject(dto)
