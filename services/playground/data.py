88cb5d3b5f10dbea86e91ba90586ebe8deee1521
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-26 23:37:58 +0800

    Revert "refactor: Remove some data classes"
    This reverts commit b53fafaa7c69c1f35b8818129876c373391fa510.

--- services/playground/data.py
+++ services/playground/data.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from common.utils.enum_util import enum_members_to_list
 
 
 class PlaySpeechDTO(BaseModel):
@@ -36,6 +38,42 @@ class FileInfo(BaseModel):
     origin_file_name: str
     file_name: str
     file_size: int  # Bytes
+
+
+class Position(BaseModel):
+    x: float = Field(description="x position")
+    y: float = Field(description="y position")
+    z: float = Field(description="z position")
+
+
+class Transform(BaseModel):
+    scale: float = Field(description="The scale of the gameobject")
+    position: Position = Field(description="The position of the gameobject")
+
+
+class GameObjectInfo(BaseModel):
+    instance_id: int
+    game_object_name: str
+    transform: Transform
+
+
+class ScaleOperationDTO(BaseModel):
+    instance_id: int
+    target_scale: float
+
+
+class GameObjectType(str, Enum):
+    CUBE = "cube"
+    SPHERE = "sphere"
+
+
+class CreateGameObjectDTO(BaseModel):
+    instance_id: int = Field(description="The id of the gameobject instance")
+    game_object_name: str = Field(description="The name of the gameobject")
+    object_type: GameObjectType = Field(
+        description=f"The type of the gameobject, can be only in {enum_members_to_list(GameObjectType)}")
+    color: str = Field(description='The color of the gameobject, hex format: "#000000"')
+    transform: Transform = Field(description="The transform of the gameobject")
 
 
 class ShowUserTextInputDTO(BaseModel):
