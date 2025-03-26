d26f0df48b360a6852ef0109147e38b8d4a9ce3d
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-26 23:37:55 +0800

    Revert "test: Test connection and audio playing"
    This reverts commit f9da3e542cf7582a5f4b258eded647b1cb36c9a0.

--- tests/test_playground.py
+++ tests/test_playground.py
@@ -1,33 +1,103 @@
 import asyncio
 import time
-from typing import Callable
+from asyncio import TaskGroup
 
+import pytest
+
+from common.config import get_config
+from common.data import LoadLive2DModelDTO, CreateGameObjectDTO, GameObjectType, Transform, Position, ScaleOperationDTO
 from common.killable_thread import KillableThread
-from event.eventemitter import emitter
+from manager.model_manager import ModelManager
 from services.playground.bridge import PlaygroundBridge
+from util import syncwait, connect
 
-pb = PlaygroundBridge(host="127.0.0.1", port=11000, password="123456")
+_config = get_config()
+_bridge = PlaygroundBridge(_config.service.playground)
+
+auto_close_flag = False
+
+_bridge_thread = KillableThread(target=_bridge.start, daemon=True)
 
 
-def waituntil(predicate: Callable[[], bool]):
-    while not predicate():
+def wait_conn():
+    while not _bridge.is_connected:
+        time.sleep(0.1)
+
+
+def block_forever():
+    while True:
         time.sleep(0.1)
 
 
 def test_conn():
-    def run():
-        asyncio.run(emitter.start())
+    _bridge_thread.start()
+    wait_conn()
+    _bridge.stop()
+    _bridge_thread.join()
+    print("Test passed")
 
-    thread_emitter = KillableThread(target=run, daemon=True)
-    thread_emitter.start()
 
-    thread_pb = KillableThread(target=pb.start, daemon=True)
-    thread_pb.start()
+# You should put at least 1 3D-model file under `../resources/static/models/3d`,
+# Or the test case will not work.
+# You can also change the path if you want.
+def test_load_3d_model():
+    _bridge_thread.start()
+    _manager = ModelManager("../resources/static/models/3d")
+    _manager.scan()
+    file_id = _manager.get_files()[0]["id"]
+    file_info = _manager.get_file_by_id(file_id)
+    wait_conn()
 
-    waituntil(lambda: pb.is_connected)
-    time.sleep(2)
-    pb.play_speech(bot_id="Koneko", audio_path="tests/resources/tts-test.wav", transcript="我是赤川鹤鸣！",
-                   bot_name="赤川小喵")
+    time.sleep(3)
+    _bridge.load_3d_model(file_info)
+    block_forever()
+    _bridge.stop()
+    _bridge_thread.join()
 
-    thread_emitter.join()
-    thread_pb.join()
+
+# You should set Live2D-model file path in your config.yaml
+# Or the test case will not work.
+def test_load_live2d_model():
+    _bridge_thread.start()
+    model_dir = _config.service.playground.model_dir
+    bot_id = _config.service.playground.bot_id
+    bot_name = _config.character.bot_name
+    wait_conn()
+    _bridge.load_live2d_model(LoadLive2DModelDTO(bot_id=bot_id, model_dir=model_dir, bot_display_name=bot_name))
+    block_forever()
+
+
+async def create_sphere():
+    await _bridge.create_gameobject(
+        CreateGameObjectDTO(instance_id=114, game_object_name="MySphere", object_type=GameObjectType.SPHERE,
+                            color="#114514", transform=Transform(scale=5.0, position=Position(x=1, y=1, z=1))))
+
+
+@pytest.mark.asyncio
+async def test_create_gameobject():
+    async with TaskGroup() as tg:
+        tg.create_task(connect(_bridge, auto_close_flag))
+        await syncwait(_bridge)
+        await create_sphere()
+
+
+@pytest.mark.asyncio
+async def test_modify_game_object_scale():
+    async with TaskGroup() as tg:
+        tg.create_task(connect(_bridge, auto_close_flag))
+        await syncwait(_bridge)
+        await create_sphere()
+        await asyncio.sleep(1)
+        for e in _bridge.get_gameobjects_info():
+            print(e.game_object_name)
+            if e.game_object_name == "MySphere":
+                await _bridge.modify_game_object_scale(ScaleOperationDTO(instance_id=e.instance_id, target_scale=0.5))
+
+
+@pytest.mark.asyncio
+async def test_query_game_objects_info():
+    async with TaskGroup() as tg:
+        tg.create_task(connect(_bridge, auto_close_flag))
+        await syncwait(_bridge)
+        await create_sphere()
+        await _bridge.query_update_gameobjects_info()
