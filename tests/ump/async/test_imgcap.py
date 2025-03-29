131218c7ce1abef18b584c59666ee0296ba65e16
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-03-29 15:36:27 +0800

    test: test_imgcap passed

---
+++ tests/ump/async/test_imgcap.py
@@ -0,0 +1,16 @@
+import pytest
+from zerolan.data.pipeline.img_cap import ImgCapQuery
+
+from private import base_url
+from ump.asyncio.pipeline.img_cap import ImgCapPipeline
+
+_imgcap = ImgCapPipeline(base_url=base_url, model_id="Salesforce/blip-image-captioning-large")
+
+
+@pytest.mark.asyncio
+async def test_imgcap():
+    query = ImgCapQuery(img_path="resources/imgcap-test.png")
+    prediction = await _imgcap.predict(query)
+    assert prediction, f"Test failed: No response."
+    print(prediction.caption)
+    assert "girl" in prediction.caption, f"Test failed: Wrong result."
