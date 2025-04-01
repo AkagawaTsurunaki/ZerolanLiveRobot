f1e1374fd9f79ec29ac384db4113008147c5bead
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-04-01 22:03:59 +0800

    refactor: Remove unused functions

--- common/utils/str_util.py
+++ common/utils/str_util.py
@@ -8,15 +8,6 @@ def is_blank(s: str) -> bool:
     return s is None or not s.strip() or s == ""
 
 
-# def split_by_punctuations(text, punctuations=None, k=10):
-#     if punctuations is None:
-#         punctuations = ["：", "。", "！", "？", "!", "?"]
-#     pattern = '|'.join(re.escape(p) for p in punctuations)
-#     parts = re.split(pattern, text)
-#     parts = [part for part in parts if part.strip()]
-#
-#     return parts
-
 def split_by_punc(text: str, lang: Language) -> List[str]:
     if lang == Language.ZH:
         cut_punc = "，。！？"
@@ -38,35 +29,3 @@ def split_by_punc(text: str, lang: Language) -> List[str]:
         return texts
 
     return punc_cut(text, cut_punc)
-
-
-def adjust_strings(strings, k):
-    adjusted_strings = []
-    i = 0
-    while i < len(strings):
-        current_str = strings[i]
-
-        # Check if current string meets the length requirement
-        if len(current_str) > k:
-            adjusted_strings.append(current_str)
-            i += 1
-        else:
-            # Combine with the previous string if possible
-            if i > 0:
-                previous_str = adjusted_strings.pop()
-                combined_str = previous_str + current_str
-                adjusted_strings.append(combined_str)
-                i += 1
-            else:
-                # If no previous string to combine with, combine with the next one
-                if i < len(strings) - 1:
-                    next_str = strings[i + 1]
-                    combined_str = current_str + next_str
-                    adjusted_strings.append(combined_str)
-                    i += 2  # Move to the string after next
-                else:
-                    # If it's the last string and still not adjusted, just append
-                    adjusted_strings.append(current_str)
-                    i += 1
-
-    return adjusted_strings
--- common/utils/web_util.py
+++ common/utils/web_util.py
@@ -1,35 +1,3 @@
-def urljoin(host: str, port: int, path: str = None):
-    """
-    根据协议、主机、端口号和路径，拼接 URL。
-    Args:
-        host: 主机，例如 127.0.0.1。
-        port: 端口号，例如 11451。
-        path: 路径，例如 /test/speak。
-
-    Returns:
-
-    """
-    assert host and port
-    assert isinstance(host, str) and isinstance(port, int)
-
-    if ("http://" not in host) and ("https://" not in host):
-        url = f"http://{host}:{port}"
-    else:
-        url = f"{host}:{port}"
-
-    if path:
-        assert isinstance(path, str)
-        assert path[0] == '/', f'"path" should begin with "/".'
-        url += path
-
-    return url
-
-
-def is_valid_url(url: str | None) -> bool:
-    protocol = url.split("://")[0]
-    return protocol in ['http', 'https']
-
-
 import netifaces as ni
 
 
