9cbf16f86b7f303f5eaf40a6b9017309e0570cd9
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-04-12 20:30:52 +0800

    feat: smart mic based on webrtcvad

---
+++ tests/devices/test_mic.py
@@ -0,0 +1,57 @@
+import asyncio
+from asyncio import TaskGroup
+
+import pytest
+from zerolan.data.pipeline.asr import ASRStreamQuery
+
+from common.concurrent.killable_thread import KillableThread
+from event.event_data import SpeechEvent
+from event.event_emitter import emitter
+from manager.config_manager import get_config
+from microphone import SmartMicrophone
+from pipeline.asr.asr_sync import ASRSyncPipeline
+
+
+@pytest.fixture(scope="session")
+def event_loop(event_loop_policy):
+    # Needed to work with asyncpg
+    loop = event_loop_policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+_config = get_config()
+mic = SmartMicrophone()
+_asr = ASRSyncPipeline(_config.pipeline.asr)
+t = KillableThread(target=mic.start, daemon=True)
+_asr_res = []
+tasks = []
+_flag = True
+
+
+@emitter.on(SpeechEvent.type)
+def _on_speech(event: SpeechEvent):
+    query = ASRStreamQuery(
+        is_final=True,
+        audio_data=event.speech,
+        media_type=event.audio_type.value,
+        sample_rate=event.sample_rate,
+        channels=event.channels,
+    )
+    for prediction in _asr.stream_predict(query):
+        print(prediction.transcript)
+        _asr_res.append(prediction)
+    if len(_asr_res) > 3:
+        global _flag
+        _flag = False
+
+
+@pytest.mark.asyncio
+async def test_vad():
+    t.start()
+    async with TaskGroup() as tg:
+        tg.create_task(emitter.start())
+        while _flag:
+            await asyncio.sleep(0.1)
+        t.kill()
+        await emitter.stop()
