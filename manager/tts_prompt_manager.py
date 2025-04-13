821006d563c7a02e7f902513f9a0357d98011605
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-04-13 15:44:57 +0800

    refactor: TTSPromptManager support local or remote prompts

--- manager/tts_prompt_manager.py
+++ manager/tts_prompt_manager.py
@@ -17,6 +17,47 @@ def _get_all_files(prompts_dir: str):
     return res
 
 
+def parse_prompt(audio_path: str):
+    audio_path = audio_path.replace("\\", "/")
+    filename = audio_path.split("/")[-1]
+    lang, sentiment, transcript = parse_tts_prompt_filename(filename)
+    tts_prompt = TTSPrompt(audio_path=audio_path, lang=lang, sentiment=sentiment,
+                           prompt_text=transcript)
+    return tts_prompt
+
+
+def parse_tts_prompt_filename(s: str) -> Tuple[str, str, str]:
+    """
+    For example:
+        [zh][开心]大家好，我是绫地宁宁，大家一起 0721 吧！.wav
+        [ja][羞恥]わ、わたしの...わたしのオナニーを見てください！.wav
+        [en][anxiety] This is a detonator, you can't press it! .wav
+    :param s: Filename
+    :return: lang, sentiment, transcript
+    """
+    matches = re.findall(r'\[(.*?)\]', s)
+
+    try:
+        lang = Language.value_of(matches[0])
+    except ValueError:
+        raise ValueError(f'Invalid language tag in string "{s}"')
+    except IndexError:
+        raise ValueError(f"The language tag could not be found in the string: {s}")
+
+    try:
+        sentiment = matches[1]
+    except ValueError:
+        raise ValueError(f'Invalid sentiment tag in string "{s}"')
+    except IndexError:
+        raise ValueError(f"The language tag could not be found in the string: {s}")
+
+    raw_filename = s.replace(f"[{lang}]", "").replace(f"[{sentiment}]", "")
+    filetype = raw_filename.split(".")[-1]
+    transcript = raw_filename[:-len(f".{filetype}")]
+
+    return lang, sentiment, transcript
+
+
 class TTSPromptManager:
     def __init__(self, config: SpeechConfig):
         self.default_tts_prompt: TTSPrompt | None = None
@@ -45,70 +86,29 @@ class TTSPromptManager:
         self.sentiments = []
         self.default_tts_prompt = None
         if self._is_remote:
-            self._load(filenames=self._remote_files, prompts_dir=None)
+            self._load(self._remote_files)
         else:
-            self._load(filenames=_get_all_files(self._prompts_dir), prompts_dir=self._prompts_dir)
+            self._load(_get_all_files(self._prompts_dir))
 
-    def _load(self, filenames: List[str], prompts_dir: str | None = None):
-        for filename in filenames:
-            remote_file = filename
-            filename = filename.replace("\\", "/")
-            if "/" in filename:
-                filename = filename.split("/")[-1]
+    def _load(self, audio_paths: List[str]):
+        for audio_path in audio_paths:
             try:
-                lang, sentiment, transcript = self.parse_tts_prompt_filename(filename)
+                prompt = parse_prompt(audio_path)
+                if prompt.lang != self._lang:
+                    continue
+                if not self._is_remote:
+                    assert os.path.exists(audio_path), f"File not found: {audio_path}"
+                    prompt.audio_path = os.path.abspath(audio_path)
+
+                if prompt.sentiment == "Default":
+                    self.default_tts_prompt = prompt
+                self.tts_prompts.append(prompt)
+                self.sentiments.append(prompt.sentiment)
             except ValueError:
-                logger.warning(f"No suitable filename parsing strategy, the audio file will skip: {filename}")
+                logger.warning(f"No suitable filename parsing strategy, the audio file will skip: {audio_path}")
                 continue
-            if lang != self._lang:
-                continue
-
-            if prompts_dir is None:
-                audio_path = remote_file
-            else:
-                audio_path = str(os.path.join(prompts_dir, filename))
-                audio_path = os.path.abspath(audio_path)
-            tts_prompt = TTSPrompt(audio_path=audio_path, lang=lang, sentiment=sentiment,
-                                   prompt_text=transcript)
-            self.tts_prompts.append(tts_prompt)
-            self.sentiments.append(sentiment)
-            if tts_prompt.sentiment == "Default":
-                self.default_tts_prompt = tts_prompt
-
+        if len(self.tts_prompts) <= 0:
+            raise RuntimeError("There are no eligible TTS prompts in the directory you provided.")
         # Output sentiments result
         sentiments = [tts_prompt.sentiment for tts_prompt in self.tts_prompts]
         logger.info(f"{len(self.tts_prompts)} TTS prompts ({self._lang}) loaded: {sentiments}")
-        if len(self.tts_prompts) <= 0:
-            raise RuntimeError("There are no eligible TTS prompts in the directory you provided.")
-
-    @staticmethod
-    def parse_tts_prompt_filename(s: str) -> Tuple[str, str, str]:
-        """
-        For example:
-            [zh][开心]大家好，我是绫地宁宁，大家一起 0721 吧！.wav
-            [ja][羞恥]わ、わたしの...わたしのオナニーを見てください！.wav
-            [en][anxiety] This is a detonator, you can't press it! .wav
-        :param s: Filename
-        :return: lang, sentiment, transcript
-        """
-        matches = re.findall(r'\[(.*?)\]', s)
-
-        try:
-            lang = Language.value_of(matches[0])
-        except ValueError:
-            raise ValueError(f'Invalid language tag in string "{s}"')
-        except IndexError:
-            raise ValueError(f"The language tag could not be found in the string: {s}")
-
-        try:
-            sentiment = matches[1]
-        except ValueError:
-            raise ValueError(f'Invalid sentiment tag in string "{s}"')
-        except IndexError:
-            raise ValueError(f"The language tag could not be found in the string: {s}")
-
-        raw_filename = s.replace(f"[{lang}]", "").replace(f"[{sentiment}]", "")
-        filetype = raw_filename.split(".")[-1]
-        transcript = raw_filename[:-len(f".{filetype}")]
-
-        return lang, sentiment, transcript
