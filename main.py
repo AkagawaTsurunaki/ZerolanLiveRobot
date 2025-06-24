9bf331edfdb21c11ed99d0cc3b2cd44e067fc97b
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-06-24 22:59:53 +0800

    feat: Print more info during initializing ZerolanLiveBot

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import asyncio
 
 from bot import ZerolanLiveRobot
+from loguru import logger
+
+from common.concurrent.abs_runnable import stop_all_runnable
 
 
 async def main():
-    bot = ZerolanLiveRobot()
-    await bot.start()
+    try:
+        bot = ZerolanLiveRobot()
+        await bot.start()
+        bot.stop()
+    except Exception as e:
+        logger.exception(e)
+        logger.error("❌️ Zerolan Live Robot exited abnormally!")
 
 
 if __name__ == '__main__':
