6b8d30d7fdfd7676612a5bbf28cc7e33e99c4230
AkagawaTsurunaki <965145619@qq.com>
2025-12-29 15:45:11 +0800

    test: Live2D lip sync passed

--- tests/services/test_live2d.py
+++ tests/services/test_live2d.py
@@ -1,27 +1,34 @@
-import os.path
-import time
+import asyncio
 from pathlib import Path
 
+import pytest
+from loguru import logger
+
 from common.concurrent.killable_thread import KillableThread
-from manager.config_manager import get_project_dir
+from manager.config_manager import get_project_dir, get_config
 from services.live2d.live2d_viewer import Live2DViewer
 from services.live2d.config import Live2DViewerConfig
 
 _dir = get_project_dir()
-_model_file = os.path.join(_dir,
-                           r"resources/static/models/live2d/hiyori_pro_zh/hiyori_pro_zh/runtime/hiyori_pro_t11.model3.json")
-_audio_path = "resources/tts-test.wav"
+print(_dir)
+_config = get_config()
+_audio_path = Path(_dir).joinpath("tests/resources/tts-test.wav")
 
 
-def test_live2d():
+@pytest.mark.asyncio
+async def test_live2d():
     config = Live2DViewerConfig(
-        model3_json_file=_model_file)
+        model3_json_file=_config.service.live2d_viewer.model3_json_file)
     viewer = Live2DViewer(config)
     t = KillableThread(target=viewer.start)
     t.start()
-    time.sleep(1)
+    await asyncio.sleep(1)
+    logger.info("First speech")
     viewer.sync_lip(Path(_audio_path))
-    time.sleep(2)
+    await asyncio.sleep(2)
+    logger.info("Second speech")
     viewer.sync_lip(Path(_audio_path))
-    time.sleep(10)
+    await asyncio.sleep(10)
+    logger.info("Stopping")
+    viewer.stop()
     t.kill()
