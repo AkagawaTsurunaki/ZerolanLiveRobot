b1ddf703206cb113018e416c2891002d567f1622
AkagawaTsurunaki <965145619@qq.com>
2025-12-29 11:20:38 +0800

    fix: bot.stop() should be awaited

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import asyncio
 from bot import ZerolanLiveRobot
 from loguru import logger
 
-from common.concurrent.abs_runnable import stop_all_runnable
-
 
 async def main():
     try:
         bot = ZerolanLiveRobot()
         await bot.start()
-        bot.stop()
+        await bot.stop()
     except Exception as e:
         logger.exception(e)
         logger.error("❌️ Zerolan Live Robot exited abnormally!")
